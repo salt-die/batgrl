0f729aef929f1aeda4b127aab62ed1381d31fa95
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-19 13:46:43 -0500

    Initial commit.

---
+++ nurses_2/layouts/layout.py
@@ -0,0 +1,6 @@
+from ..panel import Panel
+
+
+class Layout(Panel):
+    """Generic container of TUI elements.
+    """
---
+++ nurses_2/screen.py
@@ -0,0 +1,3 @@
+class Screen:
+    """Contents of the screen stored as two-dimensional numpy array of string objects.
+    """
