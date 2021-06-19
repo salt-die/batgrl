be700242b73a58f1ddd8d6f1149e24234affa935
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-19 18:31:58 -0500

    Multiple micro-optimizations.

--- nurses_2/app.py
+++ nurses_2/app.py
@@ -10,9 +10,9 @@ from prompt_toolkit.output import create_output
 
 from .widgets import Widget
 
-FLUSH_INTERVAL = .05
-POLL_INTERVAL = .5
-REFRESH_INTERVAL = 0
+ESCAPE_TIMEOUT = .05  # Seconds before we flush an escape character in the input queue.
+POLL_INTERVAL = .5  # Seconds between polling for resize events.
+REFRESH_INTERVAL = 0  # Seconds between screen redraws.
 
 
 class App(ABC):
@@ -37,10 +37,8 @@ class App(ABC):
             pass
 
     def exit(self):
-        for task in self._tasks:
+        for task in asyncio.all_tasks():
             task.cancel()
-
-        self._flush_task.cancel()
 
     def _on_resize(self):
         """Adjust widget geometry and redraw the screen.
@@ -53,6 +51,8 @@ class App(ABC):
         """
 
     async def _run_async(self):
+        """Create root widget and schedule input reading, size polling, auto-refreshing, and finally, `on_start`.
+        """
         with create_environment() as (env_out, env_in):
             self.env_out = env_out
             self.env_in = env_in
@@ -60,8 +60,8 @@ class App(ABC):
             self.kb = kb = KeyBindings()
             self.key_processor = key_processor = KeyProcessor(kb)
 
-            self._tasks = tasks = [ ]
-            self._flush_task = asyncio.create_task(_dummy_coroutine())
+            loop = asyncio.get_event_loop()
+            flush_timer = asyncio.TimerHandle(0, lambda: None, (), loop)  # dummy handle; this so we can avoid a conditional in `read_from_input`
 
             self.root = self.build()
             # assert isinstance(self.root, Widget), f"expected Widget, got {type(self.root).__name__}"  # TODO: Uncomment once widgets are implemented.
@@ -74,8 +74,9 @@ class App(ABC):
                 key_processor.feed_multiple(keys)
                 key_processor.process_keys()
 
-                self._flush_task.cancel()
-                self._flush_task = asyncio.create_task(flush_soon())
+                nonlocal flush_timer
+                flush_timer.cancel()
+                flush_timer = loop.call_later(ESCAPE_TIMEOUT, flush_input)
 
             def flush_input():
                 """Flush input.
@@ -85,36 +86,35 @@ class App(ABC):
                 key_processor.feed_multiple(keys)
                 key_processor.process_keys()
 
-            async def flush_soon():
-                await asyncio.sleep(FLUSH_INTERVAL)
-                flush_input()
-
             async def poll_size():
                 """Poll terminal dimensions every `POLL_INTERVAL` seconds and resize if dimensions have changed.
                 """
                 size = env_out.get_size()
+                on_resize = self._on_resize
 
                 while True:
                     await asyncio.sleep(POLL_INTERVAL)
 
                     new_size = env_out.get_size()
                     if size != new_size:
-                        self._on_resize()
+                        on_resize()
                         size = new_size
 
             async def auto_refresh():
                 """Redraw the screen every `REFRESH_INTERVAL` seconds.
                 """
+                refresh = self._refresh
+
                 while True:
                     await asyncio.sleep(REFRESH_INTERVAL)
-                    self._refresh()
+                    refresh()
 
             with env_in.raw_mode(), env_in.attach(read_from_input):
-                tasks.append( asyncio.create_task(poll_size()) )
-                tasks.append( asyncio.create_task(auto_refresh()) )
-                main = asyncio.create_task(self.on_start())
-                tasks.append(main)
-                await main
+                await asyncio.gather(
+                    poll_size(),
+                    auto_refresh(),
+                    self.on_start(),
+                )
 
 
 ################################################# W ###
@@ -129,24 +129,21 @@ Binding.call = call                             # G ###
 @contextmanager
 def create_environment():
     env_out = create_output()
-    env_in = create_input()
-
     env_out.enter_alternate_screen()
     env_out.hide_cursor()
     env_out.flush()
 
+    env_in = create_input()
     env_in.flush_keys()  # Ignoring type-ahead
 
     try:
         yield env_out, env_in
     finally:
-        env_out.flush()
-        env_out.quit_alternate_screen()
-
         env_in.flush_keys()
         env_in.close()
 
-async def _dummy_coroutine():
-    """Used to initialize dummy tasks.
-    """
-    pass
+        env_out.scroll_buffer_to_prompt()
+        env_out.quit_alternate_screen()
+        env_out.disable_mouse_support()
+        env_out.show_cursor()
+        env_out.flush()
