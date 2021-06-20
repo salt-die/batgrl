803e9885c193857833099c3601171efe9ee366c3
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-20 18:44:59 -0500

    `show_cursor` incorrectly stops cursor blinking.  All new attributes are set in alternate screen so once we leave we don't need to reset.

--- nurses_2/app.py
+++ nurses_2/app.py
@@ -146,7 +146,4 @@ def create_environment():
 
         env_out.scroll_buffer_to_prompt()
         env_out.quit_alternate_screen()
-        env_out.disable_mouse_support()
-        env_out.reset_attributes()
-        env_out.show_cursor()
         env_out.flush()
