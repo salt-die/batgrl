a262b97ce2f88b77738328e21f3cb5d3f875a2f4
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-20 18:08:04 -0500

    Color test.

--- nurses_2/__main__.py
+++ nurses_2/__main__.py
@@ -7,7 +7,12 @@ For temporary testing.  This file will be removed.
 import asyncio
 from itertools import cycle
 
+from prompt_toolkit.styles import Attrs
+
 from .app import App
+
+RED = Attrs(color='ff0000', bgcolor='', bold=False, underline=False, italic=False, blink=False, reverse=False, hidden=False)
+BLUE = Attrs(color='0000ff', bgcolor='', bold=False, underline=False, italic=False, blink=False, reverse=False, hidden=False)
 
 
 class MyApp(App):
@@ -18,7 +23,10 @@ class MyApp(App):
 
     async def on_start(self):
         env_out = self.env_out
+        depth = env_out.get_default_color_depth()
+
         some_text = cycle('NURSES!')
+        colors = cycle((RED, BLUE))
 
         while True:
             rows, columns = env_out.get_size()
@@ -27,13 +35,13 @@ class MyApp(App):
                 env_out.cursor_goto(y, 0)
 
                 for x in range(columns):
-                    # env_out.write_raw("escape sequence")
+                    env_out.set_attributes(next(colors), depth)
                     env_out.write(next(some_text))
 
             next(some_text)
 
             env_out.flush()
 
-            await asyncio.sleep(.05)
+            await asyncio.sleep(0)
 
 MyApp().run()
