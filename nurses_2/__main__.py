3e3a450a31033348a6ec62f8b7cfe7743bd70f7d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-20 20:11:24 -0500

    Bouncing panels.

--- nurses_2/__main__.py
+++ nurses_2/__main__.py
@@ -17,6 +17,19 @@ RED = Attrs(color='ff0000', bgcolor='', bold=False, underline=False, italic=Fals
 BLUE = Attrs(color='0000ff', bgcolor='', bold=False, underline=False, italic=False, blink=False, reverse=False, hidden=False)
 
 
+class VelocityPanel(Panel):
+    def __init__(self, dim, velocity):
+        super().__init__(dim)
+        self.velocity = velocity / abs(velocity)
+        self.pos = self.top + self.left * 1j
+
+    def update(self):
+        self.pos += self.velocity
+
+        self.top = int(self.pos.real)
+        self.left = int(self.pos.imag)
+
+
 class MyApp(App):
     def build(self):
         self.kb.add('escape')(lambda event: self.exit())
@@ -24,14 +37,14 @@ class MyApp(App):
         colors = cycle((RED, BLUE))
         some_text = cycle('NURSES!')
 
-        panel_one = Panel((20, 20))
+        panel_one = VelocityPanel((20, 20), 1 + 1j)
 
         for i in range(20):
             for j in range(20):
                 panel_one.content[i, j] = next(some_text)
                 panel_one.attrs[i, j] = next(colors)
 
-        panel_two = Panel((10, 30))
+        panel_two = VelocityPanel((10, 30), 1 - 1j)
 
         for i in range(10):
             for j in range(30):
@@ -41,18 +54,35 @@ class MyApp(App):
         self.screen.panels.extend((panel_one, panel_two))
 
     async def on_start(self):
-        panel_one, panel_two = self.screen.panels
-        h, w = self.screen.dim
+        screen = self.screen
+        panel_one, panel_two = screen.panels
 
-        while True:
-            panel_one.content = np.roll(panel_one.content, 1, (0, ))
-            panel_one.attrs = np.roll(panel_one.attrs, 1, (0, ))
-            panel_one.top = (panel_one.top + 1) % h
+        async def bounce():
+            while True:
+                for panel in screen.panels:
+                    panel.update()
+                    if panel.top < 0 or panel.bottom > screen.height:
+                        panel.velocity = -panel.velocity.conjugate()
 
-            panel_two.content = np.roll(panel_two.content, 1, (1, ))
-            panel_two.attrs = np.roll(panel_two.attrs, 1, (1, ))
-            panel_two.left = (panel_two.left + 1) % w
+                    if panel.left < 0 or panel.right > screen.width:
+                        panel.velocity = panel.velocity.conjugate()
 
-            await asyncio.sleep(.05)
+                await asyncio.sleep(.05)
+
+        async def roll():
+            while True:
+                panel_one.content = np.roll(panel_one.content, 1, (0, ))
+                panel_one.attrs = np.roll(panel_one.attrs, 1, (0, ))
+
+                panel_two.content = np.roll(panel_two.content, 1, (1, ))
+                panel_two.attrs = np.roll(panel_two.attrs, 1, (1, ))
+
+                await asyncio.sleep(.11)
+
+        await asyncio.gather(
+            bounce(),
+            roll(),
+        )
+
 
 MyApp().run()
