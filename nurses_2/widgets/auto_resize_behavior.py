6ac19d34b2b87cb0ab6ed71559d4e4a340e13b01
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-24 00:11:34 -0500

    `dim` parameter is redundant for `AutoResizeBehavior` -- removed.

--- nurses_2/widgets/auto_resize_behavior.py
+++ nurses_2/widgets/auto_resize_behavior.py
@@ -8,12 +8,15 @@ class AutoResizeBehavior:
     """
     Resize to some percentage of parent (given by `size_hint`) when parent is resized.
     """
-    def __init__(self, *args, size_hint=(1.0, 1.0), **kwargs):
+    def __init__(self, pos=(0, 0), *, size_hint=(1.0, 1.0), **kwargs):
         self.h_hint, self.w_hint = size_hint
 
         assert _is_valid_hint(self.h_hint) and _is_valid_hint(self.w_hint), f'{size_hint!r} is not a valid size hint'
 
-        super().__init__(*args, **kwargs)
+        super().__init__((10, 10), pos, **kwargs)
+
+        if self.parent:
+            update_geometry(self.parent.dim)
 
     def update_geometry(self, dim):
         h_hint = self.h_hint
