1241365b40866fac7057e29f68cec7f3eb4d65f0
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-30 20:35:51 -0500

    Cleanup and rename `handle_mouse` to `create_mouse_event`.

--- nurses_2/mouse.py
+++ nurses_2/mouse.py
@@ -7,21 +7,18 @@ __all__ = (
     "handle_mouse",
 )
 
-def handle_mouse(key_press):
-    """Homogenize mouse events.
+def create_mouse_event(key_press):
+    """Create MouseEvent from a KeyPress.
     """
     # This is the mostly the same as /prompt_toolkit/key_binding/bindings/mouse.py
     # Reimplented for `nurses` to avoid `prompt_toolkit`'s KeyBinding class.
     # (`nurses` dispatches input differently.)
 
-    NORMAL_EVENTS = {
+    EVENTS = {
         32: MouseEventType.MOUSE_DOWN,
         35: MouseEventType.MOUSE_UP,
         96: MouseEventType.SCROLL_UP,
         97: MouseEventType.SCROLL_DOWN,
-    }
-
-    SGR_EVENTS = {
         (0, "M"): MouseEventType.MOUSE_DOWN,
         (0, "m"): MouseEventType.MOUSE_UP,
         (64, "M"): MouseEventType.SCROLL_UP,
@@ -29,14 +26,15 @@ def handle_mouse(key_press):
     }
 
     if key_press.key == Keys.Vt100MouseEvent:
-        # TypicaL:   "Esc[MaB*"
-        # Xterm SGR: "Esc[<64;85;12M"
-        # Urxvt:     "Esc[96;14;13M"
+        # Examples of `key_press.data`:
+        #   * Typical:   "Esc[MaB*"
+        #   * Xterm SGR: "Esc[<64;85;12M"
+        #   * Urxvt:     "Esc[96;14;13M"
         data = key_press.data
 
-        if data[2] == "M":
+        if data[2] == "M":  # Typical
             mouse_event, x, y = map(ord, data[3:])
-            mouse_event_type = NORMAL_EVENTS.get(mouse_event)
+            mouse_event_type = EVENTS.get(mouse_event)
 
             if x >= 0xDC00:
                 x -= 0xDC00
@@ -47,23 +45,20 @@ def handle_mouse(key_press):
             y -= 32
 
         else:
-            if data[2] == "<":
+            if data[2] == "<":  # Xterm SGR
                 mouse_event, x, y = map(int, data[3:-1].split(";"))
-                mouse_event_type = SGR_EVENTS.get((mouse_event, data[-1]))
+                mouse_event_type = EVENTS.get((mouse_event, data[-1]))
 
-            else:
+            else:  # Urxvt
                 mouse_event, x, y = map(int, data[2:-1].split(";"))
-                mouse_event_type = NORMAL_EVENTS.get(mouse_event)
+                mouse_event_type = EVENTS.get(mouse_event)
 
             x -= 1
             y -= 1
 
-    elif key_press.key == Keys.WindowsMouseEvent:
+    else:
         mouse_event, *coords = key_press.data.split(";")
         mouse_event_type = MouseEventType(mouse_event)
         x, y = map(int, coords)
-
-    else:
-        return key_press
 
     return MouseEvent(position=(y, x), event_type=mouse_event_type)  # y, x swapped for nurses compatibility
