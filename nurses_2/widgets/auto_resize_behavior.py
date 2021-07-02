50a00c5695ee19eaeeb67c9e1c2595920799040a
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-02 03:46:28 -0500

    Unused import removed.
    `dim` parameter optional for `AutoResizeBehavior`.

--- nurses_2/widgets/auto_resize_behavior.py
+++ nurses_2/widgets/auto_resize_behavior.py
@@ -1,5 +1,3 @@
-from .widget import Widget
-
 def _is_valid_hint(hint):
     return hint is None or 0 < hint
 
@@ -8,13 +6,15 @@ class AutoResizeBehavior:
     """
     Resize to some percentage of parent (given by `size_hint`) when parent is resized.
     """
-    def __init__(self, pos=(0, 0), *, size_hint=(1.0, 1.0), **kwargs):
+    def __init__(self, *args, size_hint=(1.0, 1.0), **kwargs):
         self.h_hint, self.w_hint = size_hint
 
         assert _is_valid_hint(self.h_hint) and _is_valid_hint(self.w_hint), f'{size_hint!r} is not a valid size hint'
 
-        super().__init__((10, 10), pos, **kwargs)
-
+        if not args:
+            super().__init__(dim=(10, 10), **kwargs)
+        else:
+            super().__init__(*args, **kwargs)
 
     def update_geometry(self):
         h_hint = self.h_hint
