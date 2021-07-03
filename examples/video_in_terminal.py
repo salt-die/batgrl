a238f84140b2bcbedab6bda8838ba26764addd6a
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-03 12:08:13 -0500

    Renamed example.

---
+++ examples/video_in_terminal.py
@@ -0,0 +1,25 @@
+"""
+Example for how to play videos with nurses.
+
+Requires opencv-python.
+"""
+from nurses_2.app import App
+from nurses_2.widgets.auto_resize_behavior import AutoResizeBehavior
+from nurses_2.widgets.video_player import VideoPlayer
+
+PATH_TO_VIDEO = ""  # "path/to/video.mp4"
+
+
+class AutoResizeVideoPlayer(AutoResizeBehavior, VideoPlayer):
+    pass
+
+
+class MyApp(App):
+    async def on_start(self):
+        player = AutoResizeVideoPlayer(path=PATH_TO_VIDEO)
+
+        self.root.add_widget(player)
+        player.start()
+
+
+MyApp().run()
