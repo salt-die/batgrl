f34e3a7d9c7915d0d6e045921927442828c800e9
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-05 16:53:56 -0500

    Doc string added.

--- nurses_2/widgets/auto_resize_behavior.py
+++ nurses_2/widgets/auto_resize_behavior.py
@@ -5,6 +5,12 @@ def _is_valid_hint(hint):
 class AutoResizeBehavior:
     """
     Resize to some percentage of parent (given by `size_hint`) when parent is resized.
+
+    Parameters
+    ----------
+    size_hint : tuple[float | None, float | None], default: (1.0, 1.0)
+        Dimension as a percentage of parent's dimension. None indicates
+        height or width attribute will be used normally.
     """
     def __init__(self, *args, size_hint=(1.0, 1.0), **kwargs):
         self.h_hint, self.w_hint = size_hint
