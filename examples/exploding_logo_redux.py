2fc75e06a80d35e5c89675d6f89a62f323abb24c
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-07 10:52:42 -0500

    Initial commit.

---
+++ examples/exploding_logo_redux.py
@@ -0,0 +1,173 @@
+"""
+Credit for ascii art logo to Matthew Barber (https://ascii.matthewbarber.io/art/python/)
+
+Directions:
+    'esc' to quit
+    'r' to reset
+    'click' to poke
+"""
+import asyncio
+from pathlib import Path
+
+import numpy as np
+import cv2
+
+from nurses_2.app import App
+from nurses_2.mouse import MouseEventType
+from nurses_2.widgets.auto_resize_behavior import AutoResizeBehavior
+from nurses_2.widgets.half_block_particle_field import HalfBlockField, HalfBlockParticle
+from nurses_2.widgets.image import Image
+
+HEIGHT, WIDTH = 18, 36
+POWER = 2
+MAX_PARTICLE_SPEED = 10
+FRICTION = .97
+
+PERCENTS = tuple(np.linspace(0, 1, 30))
+
+PATH_TO_BACKGROUND = Path('background.png')
+PATH_TO_LOGO_FULL = Path('python_discord_logo.png')
+
+
+class PokeParticle(HalfBlockParticle):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.middle_row = self.middle_column = 0
+        self.original_position = self.pos
+
+        self.position = complex(self.top, self.left)
+        self.velocity = 0j
+
+        self._update_task = self._reset_task = asyncio.create_task(asyncio.sleep(0))  # dummy task
+
+    def update_geometry(self):
+        """
+        Re-position towards center of parent's canvas.
+        """
+        old_middle_row = self.middle_row
+        old_middle_column = self.middle_column
+
+        parent_middle_row, parent_middle_column = self.parent.middle
+        self.middle_row = parent_middle_row - HEIGHT // 2
+        self.middle_column = parent_middle_column - WIDTH // 2
+
+        move_vertical = self.middle_row - old_middle_row
+        move_horizontal = self.middle_column - old_middle_column
+
+        o_top, o_left = self.original_position
+        o_top += move_vertical
+        o_left += move_horizontal
+
+        self.original_position = o_top, o_left
+        self.position += complex(move_vertical, move_horizontal)
+        self.top += move_vertical
+        self.left += move_horizontal
+
+    def on_click(self, mouse_event):
+        if mouse_event.event_type in (MouseEventType.MOUSE_DOWN, MouseEventType.MOUSE_DOWN_MOVE):
+            if dyx := -complex(*self.absolute_to_relative_coords(mouse_event.position)):
+                self.velocity += POWER * dyx / (dyx.real**2 + dyx.imag**2)
+
+                if self._update_task.done():
+                    self._reset_task.cancel()
+                    self._update_task = asyncio.create_task(self.update())
+
+    def on_press(self, key_press):
+        if key_press.key == "r" and self._reset_task.done():
+            self._reset_task = asyncio.create_task(self.reset())
+
+    async def update(self):
+        parent = self.parent
+
+        while True:
+            velocity = self.velocity
+
+            speed = abs(velocity)
+
+            if speed < .001:
+                return
+
+            if speed > MAX_PARTICLE_SPEED:
+                velocity *= MAX_PARTICLE_SPEED / speed
+
+            self.position += velocity
+            position = self.position
+
+            self.top = top = position.real
+            self.left = left = round(position.imag)
+
+            if (
+                top < 0 and velocity.real < 0
+                or top >= parent.height and velocity.real > 0
+            ):
+                velocity = -velocity.conjugate()
+
+            if (
+                left < 0 and velocity.imag < 0
+                or left >= parent.width and velocity.imag > 0
+            ):
+                velocity = velocity.conjugate()
+
+            self.velocity = velocity * FRICTION
+
+            try:
+                await asyncio.sleep(0)
+            except asyncio.CancelledError:
+                return
+
+    async def reset(self):
+        """
+        Coroutine that returns a particle to its starting position with original color.
+        """
+        self._update_task.cancel()
+
+        self.velocity = 0j
+
+        start_y, start_x = self.pos
+        end_y, end_x = self.original_position
+
+        for percent in PERCENTS:
+            percent_left = 1 - percent
+
+            self.top = percent_left * start_y + percent * end_y
+            self.left = round(percent_left * start_x + percent * end_x)
+            self.position = complex(self.top, self.left)
+
+            try:
+                await asyncio.sleep(0)
+            except asyncio.CancelledError:
+                return
+
+
+class AutoResizeHalfBlockField(AutoResizeBehavior, HalfBlockField):
+    pass
+
+
+class AutoResizeImage(AutoResizeBehavior, Image):
+    pass
+
+
+class MyApp(App):
+    async def on_start(self):
+        background = AutoResizeImage(path=PATH_TO_BACKGROUND)
+
+        path = str(PATH_TO_LOGO_FULL)
+        bgr_texture = cv2.imread(path, cv2.IMREAD_COLOR)
+        rgb_texture = cv2.cvtColor(bgr_texture, cv2.COLOR_BGR2RGB)
+        texture =  cv2.resize(rgb_texture, (WIDTH, HEIGHT * 2))
+
+        h, w, _ = texture.shape
+
+        field = AutoResizeHalfBlockField()
+
+        field.add_widgets(
+            PokeParticle(pos=(y / 2 + .25, x), color=texture[y, x], alpha=.75)
+            for y in range(h)
+            for x in range(w)
+        )
+
+        self.root.add_widgets(background, field)
+
+
+MyApp().run()
---
+++ nurses_2/widgets/half_block_particle_field.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+from .particle_field import ParticleField, Particle
+from ..colors import BLACK
+
+
+class HalfBlockField(ParticleField):
+    """
+    A widget that only has HalfBlockParticle children.
+
+    Notes
+    -----
+    HalfBlockFields are an optimized way to render many .5x1 TUI elements.
+
+    Raises
+    ------
+    TypeError if `add_widget` argument is not an instance of `HalfBlockParticle`.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.buffer = np.zeros((3, ), dtype=np.float16)
+
+    def add_widget(self, widget):
+        if not isinstance(widget, HalfBlockParticle):
+            raise TypeError(f"expected HalfBlockParticle, got {type(widget).__name__}")
+
+        super().add_widget(widget)
+
+    def render(self, canvas_view, colors_view, rect):
+        """
+        Paint region given by rect into canvas_view and colors_view.
+        """
+        buffer = self.buffer
+        subtract, multiply, add = np.subtract, np.multiply, np.add
+        t, l, _, _, h, w = rect
+
+        for child in self.children:
+            ct = child.top
+            pos = top, left = int(ct) - t, child.left - l
+
+            if 0 <= top < h and 0 <= left < w:
+                canvas_view[pos] = "▀"
+
+                color = colors_view[pos][3:] if (ct % 1) >= .5 else colors_view[pos][:3]
+                subtract(child.color, color, out=buffer, dtype=np.float16)
+                multiply(buffer, child.alpha, out=buffer)
+                add(buffer, color, out=color, casting="unsafe")
+
+
+class HalfBlockParticle(Particle):
+    """
+    A .5x1 TUI element that's Widget-like, except it has no render method.
+
+    Requires a `HalfBlockField` to be rendered.
+
+    Notes
+    -----
+    The y-component of `pos` can be a float. The fractional part determines
+    whether the half block is upper or lower.
+    """
+    def __init__(self, pos=(0, 0), *, color=BLACK, alpha=1.0):
+        self.top, self.left = pos
+        self.color = color
+        self.alpha = alpha
+        self.parent = None
+
+    def update_geometry(self):
+        """
+        Update geometry due to a change in parent's size.
+        """
