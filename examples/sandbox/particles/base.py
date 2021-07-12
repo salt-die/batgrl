732600c2d217cab7e14db15c68e4c42aea47beb9
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-12 14:50:24 -0500

    Physics based kinematics partially implemented.

--- examples/sandbox/particles/base.py
+++ examples/sandbox/particles/base.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import asyncio
-import random
+from random import random
 
 from .line import line
 
@@ -43,6 +43,26 @@ class Element(ABC):
     def sleep(self):
         self._update_task.cancel()
 
+    def wake_neighbors(self):
+        world = self.world
+        h, w = world.shape
+        y, x = self.pos
+        coords = (
+            (y - 1, x - 1),
+            (y - 1, x),
+            (y - 1, x + 1),
+            (y, x - 1),
+            (y, x + 1),
+            (y + 1, x - 1),
+            (y + 1, x),
+            (y + 1, x + 1),
+        )
+
+        for y, x in coords:
+            if 0 <= y < h and 0 <= x < w:
+                if target := world[y, x]:
+                    target.wake()
+
 
 class Solid(Element):
     def __init__(self, world, position):
@@ -64,41 +84,101 @@ class Solid(Element):
 
 
 class Movable(ABC):
-    INERTIAL_RESISTANCE = 0
     FRICTION = .8
     GRAVITY = 1 + 0j
+
+    INERTIAL_RESISTANCE = 0
     MASS = 1
 
     def __init__(self, world, position):
         self.world = world
         self.coords = complex(*position)
-        self.velocity = 0j
+        self._velocity = 4 * random() + 4 * random() * 1j
         self._update_task = asyncio.create_task(self.update())
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, value):
+        self._velocity = value
+        self.wake()
 
     @property
     def pos(self):
         pos = self.coords
         return round(pos.real), round(pos.imag)
 
-    def wake_neighbors(self):
+    def fall(self):
+        self.velocity += self.GRAVITY
+        self.velocity *= self.FRICTION
+
+        previous_yx = self.pos
+        new_coords = self.coords + self.velocity
+        target_yx = new_y, new_x = round(new_coords.real), round(new_coords.imag)
+
         world = self.world
         h, w = world.shape
-        y, x = self.pos
-        coords = (
-            (y - 1, x - 1),
-            (y - 1, x),
-            (y - 1, x + 1),
-            (y, x - 1),
-            (y, x + 1),
-            (y + 1, x - 1),
-            (y + 1, x),
-            (y + 1, x + 1),
-        )
 
-        for y, x in coords:
-            if 0 <= y < h and 0 <= x < w:
-                if target := world[y, x]:
-                    target.wake()
+        if new_y < 0:
+            new_y = 0
+        elif new_y >= h:
+            new_y = h - 1
+
+        if new_x < 0:
+            new_x = 0
+        elif new_x >= w:
+            new_x = w - 1
+
+        world[previous_yx] = None
+        for yx in line(*previous_yx, new_y, new_x):
+            if particle := world[yx]:
+                world[previous_yx] = self
+                self.coords = complex(*previous_yx)
+                return self.collide(particle)
+
+            previous_yx = yx
+
+        if previous_yx == target_yx:
+            world[target_yx] = self
+            self.coords = new_coords
+
+    def inelastic_collision(self, other):
+        ...
+
+    def elastic_collision(self, other):
+        self_mass = self.MASS
+        other_mass = other.MASS
+
+        relative_coords = self.coords - other.coords
+        relative_velocity = self.velocity - other.velocity
+
+        distance = relative_coords.real**2 + relative_coords.imag**2  # Technically, distance squared
+
+        theta = relative_velocity * relative_coords.conjugate()
+        force = 2 * theta * relative_coords / (distance * (self_mass + other_mass))
+
+        self.velocity -= other_mass * force
+        other.velocity += self_mass * force
+
+    def step(self):
+        move = self.move
+        pos = y, x = self.pos
+
+        if self.velocity:
+            self.fall()
+        elif move(pos, y + 1, x):
+            self.velocity += self.GRAVITY
+        elif not move(pos, y + 1, x + 2 * round(random()) - 1):
+            self.sleep()
+
+    @abstractmethod
+    def collide(self, other):
+        """
+        Particle collision. Elastic or in-elastic depending on
+        particle types.
+        """
 
 
 class MovableSolid(Movable, Solid):
@@ -126,13 +206,13 @@ class MovableSolid(Movable, Solid):
         self._update_task.cancel()
         return True
 
-    def step(self):
-        move = self.move
-        pos = y, x = self.pos
-        xs = x, x - 1, x + 1
-
-        if not any(move(pos, y + 1, x) for x in xs):
-            self.sleep()
+    def collide(self, other):
+        if not isinstance(other, Movable):
+            self.velocity = 0j
+        elif isinstance(other, Solid):
+            self.elastic_collision(other)
+        elif isinstance(other, Liquid):
+            self.inelastic_collision(other)
 
 
 class Liquid(Movable, Element):
@@ -156,17 +236,8 @@ class Liquid(Movable, Element):
         self._update_task.cancel()
         return True
 
-    def step(self):
-        move = self.move
-        pos = y, x = self.pos
-        xs = x, x - 1, x + 1
-
-        if not any(move(pos, y + 1, x) for x in xs):
-            # Randomly go left or right
-            if round(random.random()):
-                moved = move(pos, y, x - 1) or move(pos, y, x + 1)
-            else:
-                moved = move(pos, y, x + 1) or move(pos, y, x - 1)
-
-            if not moved:
-                self.sleep()
+    def collide(self, other):
+        if not isinstance(other, Movable):
+            self.velocity = 0j
+        else:
+            self.inelastic_collision(other)
