6c82da9804b0a4eaad422e4316e76f4c5e7d9177
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-13 16:01:26 -0500

    Buttons always release.
    ButtonContainer will return true for any mouse event that collides with it to stop Sandbox from continuing to dispatch.

--- examples/sandbox/tool_menu.py
+++ examples/sandbox/tool_menu.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-from nurses_2.colors import ColorPair, BLUE
+from nurses_2.colors import ColorPair
 from nurses_2.widgets import Widget
 from nurses_2.widgets.button_behavior import ButtonBehavior
 
+from .constants import *
 from .particles import Element
 
 
@@ -15,8 +16,9 @@ class ElementButton(ButtonBehavior, Widget):
             dim=(2, 4),
             pos=pos,
             default_color=ColorPair(0, 0, 0, *element.COLOR),
+            always_release=True,
         )
-        self.down_color = ColorPair(0, 0, 0, *(max(255, 1.2 * c) for c in element.COLOR))
+        self.down_color = ColorPair(*(max(255, 1.2 * c) for c in self.default_color))
 
     def update_down(self):
         self.colors[:, :] = self.down_color
@@ -34,8 +36,12 @@ class ButtonContainer(Widget):
 
         super().__init__(
             dim=(3 * nelements + 1, 8),
-            default_color=ColorPair(0, 0, 0, *BLUE),
+            default_color=ColorPair(0, 0, 0, *MENU_BACKGROUND_COLOR),
         )
 
         for i, element in enumerate(Element.registry.values()):
             self.add_widget(ElementButton(pos=(3 * i + 1, 2), element=element))
+
+    def on_click(self, mouse_event):
+        if self.collides_coords(mouse_event.position):
+            return True
