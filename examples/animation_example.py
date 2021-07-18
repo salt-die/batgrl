3d96df063ac47c43c8a96ab5bd5aeda9d70b9603
salt-die <53280662+salt-die@users.noreply.github.com>
2021-07-17 20:25:23 -0500

    AutoResizeBehavior added to test dynamic resizing.

--- examples/animation_example.py
+++ examples/animation_example.py
@@ -4,19 +4,20 @@ from pathlib import Path
 from nurses_2.app import App
 from nurses_2.widgets.animation import Animation, Interpolation
 from nurses_2.widgets.auto_position_behavior import Anchor, AutoPositionBehavior
+from nurses_2.widgets.auto_resize_behavior import AutoResizeBehavior
 
 
 PATH_TO_FRAMES_DIR = Path('frames')
 
 
-class AutoPositionAnimation(AutoPositionBehavior, Animation):
+class AutoGeometryAnimation(AutoResizeBehavior, AutoPositionBehavior, Animation):
     pass
 
 
 class MyApp(App):
     async def on_start(self):
-        animation = AutoPositionAnimation(
-            dim=(14, 30),
+        animation = AutoGeometryAnimation(
+            size_hint=(.5, .5),
             anchor=Anchor.CENTER,
             pos_hint=(.5, .5),
             paths=PATH_TO_FRAMES_DIR,
