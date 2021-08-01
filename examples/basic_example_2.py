7f2f6c84466e23a85aa6f438dae694cb4d3e92f4
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-01 01:35:11 -0500

    Correctly label gradients.

--- examples/basic_example_2.py
+++ examples/basic_example_2.py
@@ -12,8 +12,8 @@ WHITE_ON_RED = color_pair(WHITE, RED)
 WHITE_ON_GREEN = color_pair(WHITE, GREEN)
 WHITE_ON_BLUE = color_pair(WHITE, BLUE)
 
-LEFT_GRADIENT = gradient(BIG_WIDGET_SIZE.height, WHITE_ON_RED, WHITE_ON_GREEN)
-RIGHT_GRADIENT = gradient(BIG_WIDGET_SIZE.height, WHITE_ON_GREEN, WHITE_ON_BLUE)
+TOP_GRADIENT = gradient(BIG_WIDGET_SIZE.height, WHITE_ON_RED, WHITE_ON_GREEN)
+BOTTOM_GRADIENT = gradient(BIG_WIDGET_SIZE.height, WHITE_ON_GREEN, WHITE_ON_BLUE)
 
 
 class AutoPositionScrollView(AutoPositionBehavior, ScrollView):
@@ -26,7 +26,7 @@ class MyApp(App):
 
         for y in range(BIG_WIDGET_SIZE.height):
             big_widget.add_text(" ".join(f'({y:<2}, {x:<2})' for x in range(N)), row=y)
-            big_widget.colors[y] = gradient(BIG_WIDGET_SIZE.width, LEFT_GRADIENT[y], RIGHT_GRADIENT[y])
+            big_widget.colors[y] = gradient(BIG_WIDGET_SIZE.width, TOP_GRADIENT[y], BOTTOM_GRADIENT[y])
 
         scroll_view = AutoPositionScrollView(dim=(10, 30), anchor=Anchor.CENTER, pos_hint=(0.5, 0.5))
         scroll_view.add_widget(big_widget)
