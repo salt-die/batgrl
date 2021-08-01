7c12d53e7d6a6d4586cadf1628e29b1533166905
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-01 00:35:51 -0500

    ScrollBarSettings expanded to allow hover and active colors on indicators.
    Indicators widget-fied to simplify click dispatching and rendering.

--- nurses_2/widgets/scroll_view/scrollbar.py
+++ nurses_2/widgets/scroll_view/scrollbar.py
@@ -1,30 +1,85 @@
 from typing import NamedTuple
 
 from ...colors import BLACK, Color, color_pair
+from ...mouse import MouseEventType
 from ...widgets.behaviors.grabbable_behavior import GrabbableBehavior
 from ..widget import Widget
 
 
 class ScrollBarSettings(NamedTuple):
+    """
+    Settings for ScrollView scrollbars. `indicator_length`
+    is doubled for horizontal scrollbars.
+    """
     bar_color: Color
-    indicator_color: Color
-    indicator_width: int  # This value doubled for horizontal scrollbars.
-    # TODO: indicator_active_color, bar_active_color
+    indicator_inactive_color: Color
+    indicator_hover_color: Color
+    indicator_active_color: Color
+    indicator_length: int  # This value doubled for horizontal scrollbars.
 
 
-# TODO: ButtonBehaviors instead of GrabbableBehavior -- To change color of bar and indicator when active.
-class VerticalBar(GrabbableBehavior, Widget):
-    def __init__(self, *args, settings: ScrollBarSettings, **kwargs):
-        kwargs.pop('default_color_pair', None)
-        bar_color, indicator_color, indicator_width = settings
+class _IndicatorBehavior:
+    """
+    Common behavior for vertical and horizontal indicators.
+    """
+    def update_color(self, mouse_event):
+        if self.collides_coords(mouse_event.position):
+            self.colors[..., 3:] = self.hover_color
+        else:
+            self.colors[..., 3:] = self.inactive_color
 
-        super().__init__(*args, default_color_pair=color_pair(BLACK, bar_color), **kwargs)
+    def ungrab(self, mouse_event):
+        super().ungrab(mouse_event)
+        self.update_color(mouse_event)
 
-        self.indicator = Widget(
-            dim=(indicator_width, 2),
-            default_color_pair=color_pair(BLACK, indicator_color),
-        )
+    def on_click(self, mouse_event):
+        if (
+            not super().on_click(mouse_event)
+            and mouse_event.event_type == MouseEventType.MOUSE_MOVE
+        ):
+            self.update_color(mouse_event)
 
+
+class _VerticalIndicator(_IndicatorBehavior, GrabbableBehavior, Widget):
+    def __init__(self, inactive_color, hover_color, active_color, length):
+        super().__init__(dim=(length, 2), default_color_pair=color_pair(BLACK, inactive_color))
+        self.active_color = active_color
+        self.hover_color = hover_color
+        self.inactive_color = inactive_color
+
+    def update_geometry(self):
+        vertical_bar = self.parent
+        scroll_view = vertical_bar.parent
+
+        if scroll_view is None:
+            return
+
+        self.top = round(scroll_view.vertical_proportion * vertical_bar.fill_width)
+
+    def grab(self, mouse_event):
+        super().grab(mouse_event)
+        self.colors[..., 3:] = self.active_color
+        self._last_y = mouse_event.position[0]
+
+    def grab_update(self, mouse_event):
+        vertical_bar = self.parent
+        scroll_view = vertical_bar.parent
+
+        dy = mouse_event.position[0] - self._last_y
+        self._last_y = mouse_event.position[0]
+
+        scroll_view.vertical_proportion += dy / vertical_bar.fill_width
+
+        self.update_geometry()
+
+
+class _VerticalBar(Widget):
+    def __init__(self, settings: ScrollBarSettings):
+        bar_color, *indicator_settings = settings
+
+        super().__init__(default_color_pair=color_pair(BLACK, bar_color))
+
+        self.indicator = _VerticalIndicator(*indicator_settings)
         self.add_widget(self.indicator)
 
     def update_geometry(self):
@@ -37,34 +92,64 @@ class VerticalBar(GrabbableBehavior, Widget):
 
     @property
     def fill_width(self):
-        return self.height - self.indicator.width - self.parent.show_horizontal_bar
+        return self.height - self.indicator.height - self.parent.show_horizontal_bar
 
-    def render(self, canvas_view, colors_view, rect):
-        self.indicator.top = round(self.parent.vertical_proportion * self.fill_width)
-        super().render(canvas_view, colors_view, rect)
+    def on_click(self, mouse_event):
+        if (
+            mouse_event.event_type == MouseEventType.MOUSE_DOWN
+            and self.collides_coords(mouse_event.position)
+        ):
+            y, _ = self.absolute_to_relative_coords(mouse_event.position)
+            sv = self.parent
+
+            if y == self.height - 1 and sv.show_horizontal_bar:
+                return True
+
+            sv.vertical_proportion = y / self.fill_width
+            self.indicator.update_geometry()
+            self.indicator.grab(mouse_event)
+            return True
+
+
+class _HorizontalIndicator(_IndicatorBehavior, GrabbableBehavior, Widget):
+    def __init__(self, inactive_color, hover_color, active_color, length):
+        super().__init__(dim=(1, length << 1), default_color_pair=color_pair(BLACK, inactive_color))
+        self.active_color = active_color
+        self.hover_color = hover_color
+        self.inactive_color = inactive_color
+
+    def update_geometry(self):
+        horizontal_bar = self.parent
+        scroll_view = horizontal_bar.parent
+
+        if scroll_view is None:
+            return
+
+        self.left = round(scroll_view.horizontal_proportion * horizontal_bar.fill_width)
 
     def grab(self, mouse_event):
         super().grab(mouse_event)
-        self._last_y = mouse_event.position[0]
+        self.colors[..., 3:] = self.active_color
+        self._last_x = mouse_event.position[1]
 
     def grab_update(self, mouse_event):
-        dy = mouse_event.position[0] - self._last_y
-        self._last_y = mouse_event.position[0]
-        self.parent.vertical_proportion += dy / self.fill_width
+        horizontal_bar = self.parent
+        scroll_view = horizontal_bar.parent
+
+        dx = mouse_event.position[1] - self._last_x
+        self._last_x = mouse_event.position[1]
+
+        scroll_view.horizontal_proportion += dx / horizontal_bar.fill_width
+
+        self.update_geometry()
 
 
-class HorizontalBar(GrabbableBehavior, Widget):
-    def __init__(self, *args, settings: ScrollBarSettings, **kwargs):
-        kwargs.pop('default_color_pair', None)
-        bar_color, indicator_color, indicator_width = settings
+class _HorizontalBar(Widget):
+    def __init__(self, settings: ScrollBarSettings):
+        bar_color, *indicator_settings = settings
 
-        super().__init__(*args, default_color_pair=color_pair(BLACK, bar_color), **kwargs)
-
-        self.indicator = Widget(
-            dim=(1, indicator_width << 1),
-            default_color_pair=color_pair(BLACK, indicator_color),
-        )
-
+        super().__init__(default_color_pair=color_pair(BLACK, bar_color))
+        self.indicator = _HorizontalIndicator(*indicator_settings)
         self.add_widget(self.indicator)
 
     def update_geometry(self):
@@ -79,16 +164,18 @@ class HorizontalBar(GrabbableBehavior, Widget):
     def fill_width(self):
         return self.width - self.indicator.width - self.parent.show_vertical_bar * 2
 
-    def render(self, canvas_view, colors_view, rect):
-        self.indicator.left = round(self.parent.horizontal_proportion * self.fill_width)
-        super().render(canvas_view, colors_view, rect)
+    def on_click(self, mouse_event):
+        if (
+            mouse_event.event_type == MouseEventType.MOUSE_DOWN
+            and self.collides_coords(mouse_event.position)
+        ):
+            _, x = self.absolute_to_relative_coords(mouse_event.position)
+            sv = self.parent
 
-    def grab(self, mouse_event):
-        super().grab(mouse_event)
-        self._last_x = mouse_event.position[1]
+            if x >= self.width - 2 and sv.show_vertical_bar:
+                return True
 
-    def grab_update(self, mouse_event):
-        dx = mouse_event.position[1] - self._last_x
-        self._last_x = mouse_event.position[1]
-
-        self.parent.horizontal_proportion += dx / self.fill_width
+            sv.horizontal_proportion = x / self.fill_width
+            self.indicator.update_geometry()
+            self.indicator.grab(mouse_event)
+            return True
