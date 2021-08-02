bad22a28d4be6db35996b659a004c89d0283d34b
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-02 06:22:43 -0500

    Added DraggableBehavior as well.

--- examples/basic_example_3.py
+++ examples/basic_example_3.py
@@ -1,5 +1,5 @@
 """
-Scatter example.
+Scatter and DraggableBehavior example.
 """
 import asyncio
 
@@ -10,13 +10,26 @@ from nurses_2.colors import BLUE, GREEN, WHITE, gradient, color_pair
 from nurses_2.widgets import Widget
 from nurses_2.widgets.scatter import Scatter
 from nurses_2.widgets.behaviors import AutoSizeBehavior
+from nurses_2.widgets.behaviors.draggable_behavior import DraggableBehavior
 
 WHITE_ON_GREEN = color_pair(WHITE, GREEN)
 WHITE_ON_BLUE = color_pair(WHITE, BLUE)
 
 
-class AutoSizeScatter(AutoSizeBehavior, Scatter):
+class BorderWidget(AutoSizeBehavior, DraggableBehavior, Widget):
     ...
+
+
+class AutoGeometryScatter(Scatter):
+    def __init__(self, **kwargs):
+        super().__init__(pos=(1, 1), **kwargs)
+
+    def update_geometry(self):
+        h, w = self.parent.dim
+
+        self.resize((h - 2, w - 2))
+
+        super().update_geometry()
 
 
 class PrettyWidget(Widget):
@@ -39,10 +52,13 @@ class MyApp(App):
         widget_1 = PrettyWidget(dim=(20, 40))
         widget_2 = PrettyWidget(dim=(10, 50))
 
-        autosize_scatter = AutoSizeScatter()
-        autosize_scatter.add_widgets(widget_1, widget_2)
+        autogeometry_scatter = AutoGeometryScatter(default_color_pair=25)
+        autogeometry_scatter.add_widgets(widget_1, widget_2)
 
-        self.root.add_widget(autosize_scatter)
+        border_widget = BorderWidget(size_hint=(.75, .75), default_color_pair=WHITE_ON_BLUE)
+        border_widget.add_widget(autogeometry_scatter)
+
+        self.root.add_widget(border_widget)
 
 
 MyApp().run()
