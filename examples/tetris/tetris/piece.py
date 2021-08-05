b5235516531a530db3bbfddeebe64d17b3cfc849
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-05 13:24:20 -0500

    Pieces are not enabled by default.

--- examples/tetris/tetris/piece.py
+++ examples/tetris/tetris/piece.py
@@ -7,16 +7,16 @@ class Piece(Widget):
     """
     A widget that renders a tetromino.
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, is_transparent=True, **kwargs)
-        self.tetromino = self
+    def __init__(self, *args, is_transparent=True, is_enabled=False, **kwargs):
+        super().__init__(*args, is_transparent=is_transparent, is_enabled=is_enabled, **kwargs)
 
     @property
     def tetromino(self):
         return self._tetromino
 
     @tetromino.setter
-    def tetromino(self, new_tetromino)
+    def tetromino(self, new_tetromino):
+        self.is_enabled = True
         self._tetromino = new_tetromino
         self.orientation = Orientation.UP
         self.resize(new_tetromino.canvases[Orientation.UP].shape)
@@ -33,9 +33,9 @@ class Piece(Widget):
 
 class CenteredPiece(Piece):
     """
-    A Piece that centers itself inside a 4x4 area when resized.
+    A Piece that centers itself inside a 4x8 area when resized.
     """
     def resize(self, dim):
         super().resize(dim)
         self.top = 2 - self.height // 2
-        self.left = 2 - self.width // 2
+        self.left = 4 - self.width // 2
