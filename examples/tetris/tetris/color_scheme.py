9844b66d8db91933342df379eb2b68f920dca4af
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-07 18:31:53 -0500

    Reformatting.

--- examples/tetris/tetris/color_scheme.py
+++ examples/tetris/tetris/color_scheme.py
@@ -2,10 +2,12 @@ from nurses_2.colors import WHITE_ON_BLACK, Color
 
 CLEAR_LINE_FLASH_COLOR = WHITE_ON_BLACK
 
-J_COLOR = BLUE = Color.from_hex("130bea")
-L_COLOR = ORANGE = Color.from_hex("f46e07")
-S_COLOR = GREEN = Color.from_hex("0fea0b")
-T_COLOR = PURPLE = Color.from_hex("6900d3")
-Z_COLOR = RED = Color.from_hex("f92504")
-I_COLOR = CYAN = Color.from_hex("10f2e3")
-O_COLOR = YELLOW = Color.from_hex("eded0e")
+from_hex = Color.from_hex
+
+J_COLOR = BLUE   = from_hex("130bea")
+L_COLOR = ORANGE = from_hex("f46e07")
+S_COLOR = GREEN  = from_hex("0fea0b")
+T_COLOR = PURPLE = from_hex("6900d3")
+Z_COLOR = RED    = from_hex("f92504")
+I_COLOR = CYAN   = from_hex("10f2e3")
+O_COLOR = YELLOW = from_hex("eded0e")
