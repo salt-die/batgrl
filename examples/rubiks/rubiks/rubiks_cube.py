641bcd677c4b95d5922256c695a8b4324738b800
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-10 18:58:53 -0500

    Colors moved their own file.

--- examples/rubiks/rubiks/rubiks_cube.py
+++ examples/rubiks/rubiks/rubiks_cube.py
@@ -4,17 +4,9 @@ import numpy as np
 import cv2
 
 from nurses_2.widgets import Widget
-from nurses_2.colors import RED, YELLOW, WHITE, GREEN, BLUE, Color
 
 from .camera import Camera
 from .cube import Cube
-
-FRONT_COLOR  = RED
-BACK_COLOR   = ORANGE = Color.from_hex("f46e07")
-LEFT_COLOR   = YELLOW
-RIGHT_COLOR  = WHITE
-TOP_COLOR    = GREEN
-BOTTOM_COLOR = BLUE
 
 
 class RubiksCube(Widget):
@@ -31,8 +23,3 @@ class RubiksCube(Widget):
     def resize(self, dim):
         super().resize(dim)
         self._colors_buffer = np.zeros((2 * self.height, self.width, 3), dtype=np.uint8)
-
-    def render_cube(self, cube):
-        """
-        Render a cube to the _colors_buffer.
-        """
