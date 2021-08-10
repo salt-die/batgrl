3eb90ded110efadcd2cc6dc3003e861c73ffa903
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-09 20:36:34 -0500

    Vectorization of 4 points at a time.

--- examples/rubiks/rubiks/camera.py
+++ examples/rubiks/rubiks/camera.py
@@ -4,7 +4,7 @@ import rotation
 
 
 class Camera:
-    __slots__ = "plane",
+    __slots__ = "widget", "plane",
 
     Z_POS = -6
 
@@ -12,12 +12,19 @@ class Camera:
     INITIAL_Y_ANGLE = np.pi / 6
 
     _POSITION_BUFFER = np.zeros(3, dtype=np.float16)
-    _DIRECTION_BUFFER = np.zeros_like(_POSITION_BUFFER)
-    _PROJECTION_BUFFER = np.zeros_like(_POSITION_BUFFER)
-    _PROJECT_2D = np.zeros(2, dtype=np.float16)
+    _DIRECTIONS_BUFFER = np.zeros((4, 3), dtype=np.float16)
+    _SCALE_BUFFER = np.zeros((1, 4), dtype=np.float16)
+    _PROJECTIONS_BUFFER = np.zeros_like(_DIRECTIONS_BUFFER)
+    _POINTS_2D = np.zeros((4, 2), dtype=np.float16)
 
-    def __init__(self):
+    def __init__(self, widget):
+        self.widget = widget
         self.plane = rotation.x(self.INITIAL_X_ANGLE).copy() @ rotation.y(self.INITIAL_Y_ANGLE)
+
+    @property
+    def center(self):
+        # Note height is not divided by two -- Camera has double the height of parent's canvas.
+        return self.widget.width / 2, self.widget.height
 
     @property
     def pos(self):
@@ -27,16 +34,26 @@ class Camera:
             out=self._POSITION_BUFFER,
         )
 
-    def project(self, point):
+    def project_face(self, points):
+        """
+        Project 4 points in 3d-space to 2d-space.
+
+        `points` should be a (4, 3)-shaped np.array.
+        """
         pos = self.pos
         plane = self.plane
         xy, z = plane[:, :2], plane[:, 2]
 
-        direction = np.subtract(point, pos, out=self._DIRECTION_BUFFER)
+        directions = np.subtract(points, pos, out=self._DIRECTIONS_BUFFER)
 
-        # 1 / (z @ direction) * direction + pos
-        stretch = z @ direction
-        projection = np.divide(direction, stretch, out=self._PROJECTION_BUFFER)
-        np.add(projection, pos, out=projection)
+        scale = np.matmul(directions, z, out=self._SCALE_BUFFER)
+        projections = np.divide(directions, scale.T, out=self._PROJECTIONS_BUFFER)
+        np.add(projections, pos, out=projections)
 
-        return np.matmul(projection, xy, out=self._PROJECT_2D)
+        points2d = np.matmul(projections, xy, out=self._POINTS_2D)
+
+        # Adjust for screen coordinates
+        points2d[:, 0] = 1 + points2d[:, 0]
+        points2d[:, 1] = 1 - points2d[:, 1]
+        points2d *= self.center
+        return points2d
