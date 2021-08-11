5bf4af04020bbd92f259180c813b6921f377deb9
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-11 17:17:01 -0500

    Projection done with cv2 instead.
    Class attributes for `cv2.projectPoints` added.
    
    Properties added for z_distance and focal lengths.
    
    Rendering has option to adjust aspect ratio.

--- examples/rubiks/rubiks/camera.py
+++ examples/rubiks/rubiks/camera.py
@@ -18,61 +18,123 @@ else:
 
 
 class Camera:
-    __slots__ = "plane",
+    __slots__ = "translation", "rotation", "plane", "camera_matrix"
 
-    Z_DISTANCE = -6
+    INITIAL_Z_DISTANCE = 6.0
 
     INITIAL_X_ANGLE = np.pi / 6
     INITIAL_Y_ANGLE = np.pi / 6
+    INITIAL_Z_ANGLE = 0.0
 
-    # Buffers
-    _POSITION_BUFFER      = np.zeros(3, dtype=np.float16)
-    _DIRECTIONS_BUFFER    = np.zeros((2, 2, 2, 3), dtype=np.float16)
-    _SCALE_BUFFER         = np.zeros((1, 2, 2, 2), dtype=np.float16)
-    _PROJECTIONS_BUFFER   = np.zeros_like(_DIRECTIONS_BUFFER)
-    _POINTS_2D_BUFFER     = np.zeros((2, 2, 2, 2), dtype=np.float16)
-    _POINTS_2D_INT_BUFFER = np.zeros_like(_POINTS_2D_BUFFER, dtype=int)
-    _NORMALS_BUFFER       = np.zeros(6, dtype=np.float16)
+    CX = 0.0  # x-center of image
+    CY = 0.0  # y-center of image
+    FX = 1.0  # x-focal length
+    FY = 1.0  # y-focal length
+
+    DISTORTION_COEF = np.array([0.0, 0.0, 0.0, 0.0])
+
+    _POSITION_BUFFER = np.zeros(3, dtype=float)
+    _POINTS_2D_INT_BUFFER = np.zeros((2, 2, 2, 2), dtype=int)
+    _NORMALS_BUFFER = np.zeros(6, dtype=float)
 
     def __init__(self):
-        self.plane = rotation.x(self.INITIAL_X_ANGLE).copy() @ rotation.y(self.INITIAL_Y_ANGLE)
+        self.translation = np.array([0.0, 0.0, self.INITIAL_Z_DISTANCE])
+
+        self.rotation = np.array(
+            [
+                self.INITIAL_X_ANGLE,
+                self.INITIAL_Y_ANGLE,
+                self.INITIAL_Z_ANGLE,
+            ]
+        )
+
+        self.plane = plane = np.array([0.0, 0.0, -1.0])
+        np.matmul(rotation.x(self.INITIAL_X_ANGLE), plane, out=plane)
+        np.matmul(rotation.y(self.INITIAL_Y_ANGLE), plane, out=plane)
+        np.matmul(rotation.z(self.INITIAL_Z_ANGLE), plane, out=plane)
+
+        self.camera_matrix = np.array(
+            [
+                [self.FX,     0.0, self.CX],
+                [    0.0, self.FY, self.CY],
+                [    0.0,     0.0,     1.0],
+            ]
+        )
+
+    @property
+    def z_distance(self):
+        return self.translation[-1]
+
+    @z_distance.setter
+    def z_distance(self, distance):
+        self.translation[-1] = distance
 
     @property
     def pos(self):
         return np.multiply(
-            self.plane[:, 2],
-            self.Z_DISTANCE,
+            self.plane,
+            self.z_distance,
             out=self._POSITION_BUFFER,
         )
 
-    def render_cube(self, cube, image):
+    @property
+    def fx(self):
+        return self.camera_matrix[0, 0]
+
+    @fx.setter
+    def fx(self, value):
+        self.camera_matrix[0, 0] = value
+
+    @property
+    def fy(self):
+        return self.camera_matrix[1, 1]
+
+    @fy.setter
+    def fy(self, value):
+        self.camera_matrix[1, 1] = value
+
+    def rotate_x(self, theta):
+        self.rotation[0] += theta
+        np.matmul(rotation.x(theta), self.plane, out=self.plane)
+
+    def rotate_y(self, theta):
+        self.rotation[1] += theta
+        np.matmul(rotation.y(theta), self.plane, out=self.plane)
+
+    def rotate_z(self, theta):
+        self.rotation[2] += theta
+        np.matmul(rotation.z(theta), self.plane, out=self.plane)
+
+    def render_cube(self, cube, image, adjust_aspect=True):
         """
         Project and render a cube onto an image array.
         """
-        pos = self.pos
-        plane = self.plane
-        xy, z = plane[:, :2], plane[:, 2]
+        h, w, _ = image.shape
 
-        # Long form of `(cube.vertices - pos) / ((cube.vertices - pos) @ z)`
-        # to re-use buffers.
-        directions = np.subtract(cube.vertices, pos, out=self._DIRECTIONS_BUFFER)
+        if adjust_aspect:
+            if w > h:
+                self.fx = h / w
+                self.fy = 1.0
+            else:
+                self.fx = 1.0
+                self.fy = w / h
 
-        scale = np.matmul(directions, z, out=self._SCALE_BUFFER)
-
-        projections = np.divide(directions, scale.T, out=self._PROJECTIONS_BUFFER)
-        projections += pos
-
-        points_2d = np.matmul(projections, xy, out=self._POINTS_2D_BUFFER)
+        points_2d, _ = cv2.projectPoints(
+            cube.vertices.reshape(-1, 3),
+            self.rotation,
+            self.translation,
+            self.camera_matrix,
+            self.DISTORTION_COEF,
+        )
 
         # Translate to center and scale to image:
-        h, w, _ = image.shape
         points_2d += .5
         points_2d *= w, h
 
         vertices_2d = self._POINTS_2D_INT_BUFFER
-        vertices_2d[:] = points_2d  # Cast to int
+        vertices_2d[:] = points_2d.reshape(2, 2, 2, 2)  # Cast to int
 
-        normals = np.matmul(cube.normals, pos, out=self._NORMALS_BUFFER)
+        normals = np.matmul(cube.normals, self.pos, out=self._NORMALS_BUFFER)
 
         for normal, face, color in zip(normals, cube.faces, FACE_COLORS):
             if normal > 0:
@@ -84,7 +146,7 @@ if TESTING:
 
     from cube import Cube
 
-    image = np.zeros((300, 300, 3), dtype=np.uint8)
+    image = np.zeros((150, 300, 3), dtype=np.uint8)
 
     cubes = [Cube(np.array(position)) for position in product((-1, 0, 1), repeat=3)]
     cam = Camera()
