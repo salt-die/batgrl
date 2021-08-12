42d8356539018c7f0a6b1b8ca57f8a58834cc7b1
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-12 14:06:22 -0500

    Simplified color definitions.

--- examples/rubiks/rubiks/face_colors.py
+++ examples/rubiks/rubiks/face_colors.py
@@ -1,21 +1,24 @@
 from nurses_2.colors import Color
 
-FRONT_COLOR  = RED = Color.from_hex("cc2808")
-BACK_COLOR   = ORANGE = Color.from_hex("f46e07")
-TOP_COLOR    = GREEN = Color.from_hex("3edd08")
-BOTTOM_COLOR = BLUE = Color.from_hex("083add")
-LEFT_COLOR   = YELLOW = Color.from_hex("e8ef13")
-RIGHT_COLOR  = WHITE = Color.from_hex("efefe8")
+from_hex = Color.from_hex
+
+RED    = from_hex("cc2808")  # Front
+ORANGE = from_hex("f46e07")  # Back
+GREEN  = from_hex("3edd08")  # Top
+BLUE   = from_hex("083add")  # Bottom
+YELLOW = from_hex("e8ef13")  # Left
+WHITE  = from_hex("efefe8")  # Right
 
 FACE_COLORS = (
-    FRONT_COLOR,
-    BACK_COLOR,
-    TOP_COLOR,
-    BOTTOM_COLOR,
-    LEFT_COLOR,
-    RIGHT_COLOR,
+    RED,
+    ORANGE,
+    GREEN,
+    BLUE,
+    YELLOW,
+    WHITE,
 )
 
+# Brighten FACE_COLORS
 SELECTED_COLORS = tuple(
     Color(
         *(85 + 2 * channel // 3 for channel in color)
