69a57d80f3439b822087e98d335dbbe3f0dafd2d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-12 13:28:44 -0500

    Softer color scheme.

--- examples/rubiks/rubiks/face_colors.py
+++ examples/rubiks/rubiks/face_colors.py
@@ -1,11 +1,11 @@
-from nurses_2.colors import RED, YELLOW, WHITE, GREEN, BLUE, Color
+from nurses_2.colors import Color
 
-FRONT_COLOR  = RED
+FRONT_COLOR  = RED = Color.from_hex("cc2808")
 BACK_COLOR   = ORANGE = Color.from_hex("f46e07")
-TOP_COLOR    = GREEN
-BOTTOM_COLOR = BLUE
-LEFT_COLOR   = YELLOW
-RIGHT_COLOR  = WHITE
+TOP_COLOR    = GREEN = Color.from_hex("3edd08")
+BOTTOM_COLOR = BLUE = Color.from_hex("083add")
+LEFT_COLOR   = YELLOW = Color.from_hex("e8ef13")
+RIGHT_COLOR  = WHITE = Color.from_hex("efefe8")
 
 FACE_COLORS = (
     FRONT_COLOR,
@@ -18,7 +18,7 @@ FACE_COLORS = (
 
 SELECTED_COLORS = tuple(
     Color(
-        *(127 + channel // 2 for channel in color)
+        *(85 + 2 * channel // 3 for channel in color)
     )
     for color in FACE_COLORS
 )
