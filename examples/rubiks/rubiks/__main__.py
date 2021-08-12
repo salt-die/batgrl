a12e4502d69abc1ee226ee3839ba085d5ff497d2
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-11 20:26:31 -0500

    Uncommented app.

--- examples/rubiks/rubiks/__main__.py
+++ examples/rubiks/rubiks/__main__.py
@@ -1,22 +1,15 @@
 from nurses_2.app import App
-from nurses_2.widgets.behaviors import AutoPositionBehavior, AutoSizeBehavior, Anchor
+from nurses_2.widgets.behaviors import AutoSizeBehavior
 
 from .rubiks_cube import RubiksCube
 
 
-class AutoGeometryRubiksCube(AutoSizeBehavior, AutoPositionBehavior, RubiksCube):
+class AutoSizeRubiksCube(AutoSizeBehavior, RubiksCube):
     ...
 
 
-# class RubiksApp(App):
-#     async def on_start(self):
-#         self.root.add_widget(
-#             AutoGeometryRubiksCube(
-#                 size_hint=(.75, .75),
-#                 pos_hint=(.5, .5),
-#                 anchor=Anchor.CENTER,
-#             )
-#         )
+class RubiksApp(App):
+    async def on_start(self):
+        self.root.add_widget( AutoSizeRubiksCube() )
 
-
-# RubiksApp().run()
+RubiksApp().run()
