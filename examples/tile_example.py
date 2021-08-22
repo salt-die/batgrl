23eb23548acb9b79fa9c15ef24f6b8fb7e42ad23
salt-die <53280662+salt-die@users.noreply.github.com>
2021-08-22 13:37:39 -0500

    Initial commit.

---
+++ examples/tile_example.py
@@ -0,0 +1,30 @@
+import asyncio
+from pathlib import Path
+
+from nurses_2.app import App
+from nurses_2.widgets.image import Image
+from nurses_2.widgets.tiled_image import TiledImage
+
+LOGO_PATH = Path("images") / "python_discord_logo.png"
+LOGO_FLAT = Path("images") / "logo_solo_flat_256.png"
+
+
+class MyApp(App):
+    async def on_start(self):
+        tile_1 = Image(size=(10, 25), path=LOGO_PATH)
+        tile_2 = Image(size=(9, 19), path=LOGO_FLAT)
+
+        tiled_image = TiledImage(size=(25, 50), tile=tile_1)
+
+        self.root.add_widget(tiled_image)
+
+        await asyncio.sleep(5)
+
+        tiled_image.tile = tile_2
+
+        await asyncio.sleep(4)
+
+        tiled_image.allow_partial_tiling = False
+
+
+MyApp().run()
