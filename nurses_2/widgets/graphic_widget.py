5d0e6975698ba0a99c79c070b96d72223c48cda3
salt-die <53280662+salt-die@users.noreply.github.com>
2021-09-16 15:53:57 -0500

    `resize` implemented. Interpolation enum added.

--- nurses_2/widgets/graphic_widget.py
+++ nurses_2/widgets/graphic_widget.py
@@ -1,3 +1,6 @@
+from enum import IntEnum
+
+import cv2
 import numpy as np
 
 from ..colors import BLACK_ON_BLACK
@@ -5,7 +8,18 @@ from ..data_structures import Point, Size
 from .widget import Widget
 
 
+class Interpolation(IntEnum):
+    NEAREST = cv2.INTER_NEAREST
+    LINEAR = cv2.INTER_LINEAR
+    CUBIC = cv2.INTER_CUBIC
+    AREA = cv2.INTER_AREA
+    LANCZOS = cv2.INTER_LANCZOS4
+
+
 class GraphicWidget(Widget):
+    """
+    Base for graphical widgets.
+    """
     def __init__(
         self,
         size: Size=Size(10, 10),
@@ -16,6 +30,7 @@ class GraphicWidget(Widget):
         is_enabled=True,
         default_char="▀",
         default_color_pair=BLACK_ON_BLACK,
+        interpolation=Interpolation.LINEAR,
     ):
         self._size = size
         self.pos = pos
@@ -26,16 +41,27 @@ class GraphicWidget(Widget):
         self.parent = None
         self.children = [ ]
 
-        self.texture = np.full((2 * self.height, self.width, 3), default_color_pair[:3], dtype=np.uint8)
+        self.texture = np.full(
+            (2 * self.height, self.width, 4),
+            (*default_color_pair[:3], 1),
+            dtype=np.uint8,
+        )
 
         self.default_char = default_char
         self.default_color_pair = default_color_pair
+
+        self.interpolation = interpolation
 
     def resize(self, size: Size):
         """
         Resize widget.
         """
-        raise NotImplementedError
+        h, w = size
+        cv2.resize(
+            self.texture,
+            (w, 2 * h),
+            interpolation=self.interpolation,
+        )
 
         for child in self.children:
             child.update_geometry()
@@ -50,8 +76,10 @@ class GraphicWidget(Widget):
         """
         Paint region given by rect into canvas_view and colors_view.
         """
-        t, l, b, r, _, _ = rect
+        t, l, b, r, h, w = rect
 
         index_rect = slice(t, b), slice(l, r)
+
+        buffer = np.zeros((2 * h, w, 3), dtype=np.float16)
 
         raise NotImplementedError
