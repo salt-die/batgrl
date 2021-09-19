c9f3ba5b1cc9ff378573ae581e45d68ad311b33c
salt-die <53280662+salt-die@users.noreply.github.com>
2021-09-18 21:28:00 -0500

    TiledImage is now child of GraphicWidget.

--- nurses_2/widgets/tiled_image.py
+++ nurses_2/widgets/tiled_image.py
@@ -2,26 +2,10 @@ from math import ceil
 
 import numpy as np
 
-from .widget import Widget
-from .image import Image
+from .graphic_widget import GraphicWidget
 
 
-class ResizeProperty:
-    def __set_name__(self, owner, name):
-        self.name = "_" + name
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-
-        return getattr(instance, self.name)
-
-    def __set__(self, instance, value):
-        instance.__dict__[self.name] = value
-        instance.resize(instance.size)
-
-
-class TiledImage(Widget):
+class TiledImage(GraphicWidget):
     """
     A tiled image.
 
@@ -40,47 +24,63 @@ class TiledImage(Widget):
         This forces the widget's height and width to be multiples of the tile's
         height and width.
     """
-    tile = ResizeProperty()
-    allow_partial_tiling = ResizeProperty()
-
-    def __init__(self, *args, tile: Image, allow_partial_tiling=True, is_transparent=True, **kwargs):
-        super().__init__(*args, is_transparent=is_transparent, **kwargs)
+    def __init__(self, *args, tile: GraphicWidget, allow_partial_tiling=True, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self._tile = tile
         self._allow_partial_tiling = allow_partial_tiling
 
         self.resize(self.size)
 
+    def _load_texture(self):
+        """
+        Texture is set in `resize` from tile.
+        """
+
+    @property
+    def tile(self):
+        return self._tile
+
+    @tile.setter
+    def tile(self, new_tile):
+        self._tile = new_tile
+        self.resize(self.size)
+
+    @property
+    def allow_partial_tiling(self):
+        return self._allow_partial_tiling
+
+    @allow_partial_tiling.setter
+    def allow_partial_tiling(self, is_allowed):
+        self._allow_partial_tiling = is_allowed
+        self.resize(self.size)
+
     def resize(self, size):
         """
         Resize widget.
         """
-        min_height, min_width = size
+        h, w = size
         tile = self.tile
 
-        vertical_repeat = ceil(min_height / tile.height)
-        horizontal_repeat = ceil(min_width / tile.width)
+        v_repeat = ceil(h / tile.height)
+        h_repeat = ceil(w / tile.width)
 
-        self._size = vertical_repeat * tile.height, horizontal_repeat * tile.width
-
-        self.canvas = np.tile(tile.canvas, (vertical_repeat, horizontal_repeat))
-        self.colors = np.tile(tile.colors, (vertical_repeat, horizontal_repeat, 1))
-        self.alpha_channels = np.tile(tile.alpha_channels, (vertical_repeat, horizontal_repeat, 1, 1))
+        texture = np.tile(tile.texture, (v_repeat, h_repeat, 1))
 
         if self.allow_partial_tiling:
             self._size = size
 
-            vertical_remainder = min_height % tile.height
-            horizontal_remainder = min_width % tile.width
+            vr = h % tile.height
+            hr = w % tile.width
 
-            vertical_slice = slice(None, (-tile.height + vertical_remainder) if vertical_remainder else None)
-            horizontal_slice = slice(None, (-tile.width + horizontal_remainder) if horizontal_remainder else None)
+            vertical_slice = slice(None, (-tile.height + vr) if vr else None)
+            horizontal_slice = slice(None, (-tile.width + hr) if hr else None)
 
-            self.canvas = self.canvas[vertical_slice, horizontal_slice].copy()
-            self.colors = self.colors[vertical_slice, horizontal_slice].copy()
-            self.alpha_channels = self.alpha_channels[vertical_slice, horizontal_slice].copy()
+            self.texture = texture[vertical_slice, horizontal_slice].copy()
+
+        else:
+            self._size = v_repeat * tile.height, h_repeat * tile.width
+            self.texture = texture
 
         for child in self.children:
             child.update_geometry()
-
-    render = Image.render
