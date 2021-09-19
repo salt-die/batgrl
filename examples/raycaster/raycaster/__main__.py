124fb2ed547374d683e035d4f5e683cbb1f76c54
salt-die <53280662+salt-die@users.noreply.github.com>
2021-09-18 22:08:35 -0500

    `load_image` modified to add alpha channel.

--- examples/raycaster/raycaster/__main__.py
+++ examples/raycaster/raycaster/__main__.py
@@ -35,9 +35,9 @@ def load_image(path):
     Load an image as numpy array from a pathlib.Path.
     """
     path_str = str(path)
-    bgr_image = cv2.imread(path_str, cv2.IMREAD_COLOR)
-    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
-    return rgb_image
+    bgr_image = cv2.imread(path_str, cv2.IMREAD_ANYCOLOR)
+    rgba_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGBA)
+    return rgba_image
 
 
 class AutoSizeCaster(AutoSizeBehavior, RayCaster):
