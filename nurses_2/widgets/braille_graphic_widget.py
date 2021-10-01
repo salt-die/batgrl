9cf1e4f07e212a1f3c416f20fe241bb3d70bc7a8
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-01 18:38:58 -0500

    Braille graphic widget takes path kwarg and loads from source.
    Widget is no-longer monochrome.

--- nurses_2/widgets/braille_graphic_widget.py
+++ nurses_2/widgets/braille_graphic_widget.py
@@ -19,36 +19,6 @@ _TO_BIN.flags.writeable = False
 
 vectorized_chr = np.vectorize(chr)
 
-def texture_to_braille(arr):
-    """
-    Convert a `(m, n)`-shaped texture array to a `(m // 4, n // 2)`-shaped
-    array of braille characters.
-
-    Example
-    -------
-           In            -->           Out
-    [0 1 0 1 1 0 1 0]           ['⢸' '⠺' '⡅' '⢵']
-    [0 1 1 1 0 0 0 1]           ['⡄' '⡾' '⢜' '⠠']
-    [0 1 0 1 1 0 1 1]
-    [0 1 0 0 1 0 0 1]
-    [0 0 0 1 0 1 0 0]
-    [0 0 1 1 0 1 0 0]
-    [1 0 1 1 1 0 0 1]
-    [1 0 1 0 0 1 0 0]
-    """
-    h, w = arr.shape
-
-    sectioned = np.swapaxes(arr.reshape(h // 4, 4, w // 2, 2), 1, 2)
-
-    ords = np.sum(
-        sectioned * _TO_BIN,
-        axis=(2, 3),
-        initial=0x2800,  # first braille ord
-        dtype=np.uint16,
-    )
-
-    return vectorized_chr(ords)
-
 
 class BrailleGraphicWidget(Widget):
     """
@@ -56,70 +26,62 @@ class BrailleGraphicWidget(Widget):
 
     Parameters
     ----------
-    interpolation : Interpolation, default: Interpolation.LINEAR
-        The interpolation used when resizing the widget's texture.
-    auto_apply_texture : bool, default: False
-        If true, `apply_texture` is called on every render.
-
-    Notes
-    -----
-    BrailleGraphicWidgets have an underlying boolean array, `texture`. This texture can be
-    applied to the canvas using the `apply_texture` method. Which converts 4x2 rectangular slices
-    of the texture into corresponding braille characters. (The texture will be 4x the widget's height
-    and 2x the widget's width.)
-
-    Resizing resets `colors` to `default_color_pair`.
+    path : pathlib.Path
+        Path to image.
     """
-    def __init__(
-        self,
-        *args,
-        interpolation=Interpolation.LINEAR,
-        auto_apply_texture=False,
-        **kwargs,
-    ):
+    def __init__(self, *args, path: Path, **kwargs):
         super().__init__(*args, **kwargs)
+        self.path = path
 
-        h, w = self.size
-        self.texture = np.zeros((4 * h, 2 * w), dtype=np.uint8)
-        self.interpolation = interpolation
-        self.auto_apply_texture = auto_apply_texture
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, value):
+        self._path = value
+        self._load_texture()
 
     def resize(self, size):
-        self._size = h, w = size
+        super().resize(size)
+        self._load_texture()
 
-        self.colors = np.full(
-            (*size, 6),
-            self.default_color_pair,
-            dtype=np.uint8,
-        )
-
-        self.texture = cv2.resize(
-            self.texture,
-            (2 * w, 4 * h),
-            interpolation=self.interpolation,
-        )
-
-        self.apply_texture()
-
-    def apply_texture(self):
-        """
-        Paint texture to canvas.
-        """
-        self.canvas = texture_to_braille(self.texture)
-
-    def load_texture(self, path: Path):
-        """
-        Load texture from path and paint to canvas.
-        """
-        img_grey = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
-        _, img_binary = cv2.threshold(img_grey, 255 >> 1, 1, cv2.THRESH_BINARY)
+    def _load_texture(self):
+        img = cv2.imread(str(self.path), cv2.IMREAD_COLOR)
+        img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 
         h, w = self.size
-        self.texture = cv2.resize(img_binary, (2 * w, 4 * h))
-        self.apply_texture()
+        img_resized = cv2.resize(img_hls, (2 * w, 4 * h))
 
-    def render(self, canvas_view, colors_view, rect):
-        if self.auto_apply_texture:
-            self.apply_texture()
+        sectioned = np.swapaxes(img_resized.reshape(h, 4, w, 2, 3), 1, 2)
 
-        super().render(canvas_view, colors_view, rect)
+        lightness = sectioned[..., 1]
+        average_lightness = np.average(lightness, axis=(2, 3))
+        where_dots = lightness > average_lightness[..., None, None]
+
+        ords = np.sum(
+            where_dots * _TO_BIN,
+            axis=(2, 3),
+            initial=0x2800,  # first braille ord
+            dtype=np.uint16,
+        )
+        self.canvas = vectorized_chr(ords)
+
+        # TODO: Probably describe what's going on here...
+        # TODO: Background needs to be weighted more than foreground.
+        ndots = where_dots.sum(axis=(2, 3))
+        ndots_neg = 8 - ndots
+
+        background = sectioned.copy()
+        background[where_dots] = 0
+        bg = background.sum(axis=(2, 3)) / ndots_neg[..., None]
+
+        foreground = sectioned.copy()
+        foreground[~where_dots] = 0
+        fg = foreground.sum(axis=(2, 3)) / ndots[..., None]
+
+        fixed_bg = np.where(~np.isin(bg, (np.nan, np.inf)), bg, fg).astype(np.uint8)
+        fixed_fg = np.where(~np.isin(fg, (np.nan, np.inf)), fg, bg).astype(np.uint8)
+
+        self.colors[..., :3] = cv2.cvtColor(fixed_fg, cv2.COLOR_HLS2RGB)
+        self.colors[..., 3:] = cv2.cvtColor(fixed_bg, cv2.COLOR_HLS2RGB)
