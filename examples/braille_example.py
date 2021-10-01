16b81608a90958d0cc7fd98c2a31d010fc6f1324
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-01 18:37:46 -0500

    Updated example. Colorified.

--- examples/braille_example.py
+++ examples/braille_example.py
@@ -1,29 +1,19 @@
 from pathlib import Path
 
-import cv2
-import numpy as np
-
 from nurses_2.app import App
 from nurses_2.widgets.braille_graphic_widget import BrailleGraphicWidget
 from nurses_2.widgets.behaviors import AutoSizeBehavior
 
-PATH_TO_TREE = Path("images") / "trees.jpg"
+IMG_SOURCE = Path("images") / "sunset.jpg"
 
 
 class AutoSizeBraille(AutoSizeBehavior, BrailleGraphicWidget):
-    def __init__(self, *args, source, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.source = source
-
-    def update_geometry(self):
-        super().update_geometry()
-        self.load_texture(self.source)
+    ...
 
 
 class MyApp(App):
     async def on_start(self):
-        braille_widget = AutoSizeBraille(source=PATH_TO_TREE)
-        self.root.add_widget(braille_widget)
+        self.root.add_widget(AutoSizeBraille(path=IMG_SOURCE))
 
 
 MyApp().run()
