8ee24b083b30cb27ddfa8a11549a41a74d20294e
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-26 16:10:14 -0500

    Data structures for future key presses added.

--- nurses_2/io/input/event_data_structures.py
+++ nurses_2/io/input/event_data_structures.py
@@ -1,6 +1,7 @@
 from typing import NamedTuple
 
 from ...data_structures import Point
+from .keys import Key
 from .mouse_data_structures import MouseEventType, MouseButton, MouseModifier
 
 __all__ = (
@@ -18,3 +19,18 @@ class MouseEvent(NamedTuple):
 
 class PasteEvent(NamedTuple):
     paste: str
+
+
+# TODO: KeyPressEvents.  Currently, for most keypresses we send single characters to `on_press` methods.
+# TODO: Move following Modifier into MouseEvent as well.
+# For alt-presses, a single character will be preceded by Key.Escape. This complicates binding alt-presses.
+# Instead, we can package everything into a KeyPressEvent.
+class Modifiers(NamedTuple):
+    shift: bool
+    alt: bool
+    ctrl: bool
+
+
+class KeyPressEvent(NamedTuple):
+    key: Key | str
+    modifiers: Modifiers
