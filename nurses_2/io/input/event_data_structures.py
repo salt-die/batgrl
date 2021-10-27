04ff3fd5b340a43d775eaf66b768187d2d132070
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-27 17:48:50 -0500

    Alias properties added to Modifiers and KeyPressEvent.

--- nurses_2/io/input/event_data_structures.py
+++ nurses_2/io/input/event_data_structures.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import NamedTuple
 
 from ...data_structures import Point
@@ -25,12 +26,32 @@ class PasteEvent(NamedTuple):
 # TODO: Move following Modifier into MouseEvent as well.
 # For alt-presses, a single character will be preceded by Key.Escape. This complicates binding alt-presses.
 # Instead, we can package everything into a KeyPressEvent.
+@cache
 class Modifiers(NamedTuple):
-    shift: bool
     alt: bool
     ctrl: bool
+    shift: bool
+
+    @property
+    def meta(self):
+        """
+        Alias for `alt`.
+        """
+        return self.alt
+
+    @property
+    def control(self):
+        """
+        Alias for `ctrl`.
+        """
+        return self.ctrl
 
 
+@cache
 class KeyPressEvent(NamedTuple):
     key: Key | str
     modifiers: Modifiers
+
+    @property
+    def mods(self):
+        return self.modifiers
