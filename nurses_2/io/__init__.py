83f961483446e66373ec3a23ad410c2b4a184d4d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-29 02:17:46 -0500

    Reorganized imports.

--- nurses_2/io/__init__.py
+++ nurses_2/io/__init__.py
@@ -1,19 +1,26 @@
 import sys
 
 from .utils import is_conemu_ansi, is_windows
-from .input.mouse_data_structures import MouseEventType, MouseButton
-from .input.event_data_structures import Mods, KeyPressEvent, MouseEvent, PasteEvent
+from .input.events import (
+    Key,
+    Mods,
+    KeyPressEvent,
+    MouseEventType,
+    MouseButton,
+    MouseEvent,
+    PasteEvent,
+)
 from .input.keys import Key
 
 __all__ = (
-    "create_io",
-    "MouseEventType",
-    "MouseButton",
+    "Key",
     "Mods",
     "KeyPressEvent",
+    "MouseEventType",
+    "MouseButton",
     "MouseEvent",
     "PasteEvent",
-    "Key",
+    "create_io",
 )
 
 def create_io():
@@ -26,7 +33,7 @@ def create_io():
     if is_windows():
         from .output.windows10 import is_win_vt100_enabled, Windows10_Output
 
-        if not (is_conemu_ansi() or is_win_vt100_enabled()):
+        if not is_conemu_ansi() or not is_win_vt100_enabled():
             raise RuntimeError("nurses_2 not supported on non-vt100 enabled terminals")
 
         from .input.win32 import win32_input
