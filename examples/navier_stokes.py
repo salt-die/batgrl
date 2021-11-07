c25d86d928431a35244e87ee1ef4c78fc7332a1e
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-07 12:38:07 -0600

    Initial commit.

---
+++ examples/navier_stokes.py
@@ -0,0 +1,107 @@
+"""
+Click to add fluid. `r` to reset.
+"""
+import numpy as np
+import cv2
+from cv2 import filter2D
+
+from nurses_2.app import App
+from nurses_2.colors import Color
+from nurses_2.io import MouseButton, MouseEvent
+from nurses_2.widgets.graphic_widget import GraphicWidget
+from nurses_2.widgets.behaviors import AutoSizeBehavior
+
+VISCOSITY = 1.3
+RHO = .21
+
+# Kernels
+CONVECTION = np.array([
+    [   0, .25,    0],
+    [ .25,  -1,  .25],
+    [   0, .25,    0],
+])
+DIFFUSION = np.array([
+    [.025,  .1, .025],
+    [  .1,  .5,   .1],
+    [.025,  .1, .025],
+])
+POISSON = np.array([
+    [   0, .25,    0],
+    [ .25,   0,  .25],
+    [   0, .25,    0],
+])
+
+WATER_COLOR = Color.from_hex("1259FF")
+
+def wrap_border(array):
+    array[:2] = array[-4: -2][::-1]
+    array[-2:] = array[2: 4][::-1]
+    array[:, :2] = array[:, -4: -2][::-1]
+    array[:, -2:] = array[:, 2: 4][::-1]
+
+
+class Fluid(AutoSizeBehavior, GraphicWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.resize(self.size)
+
+    def resize(self, size):
+        """
+        Resize widget.
+        """
+        h, w = size
+        gsize = 2 * h + 4, w + 4
+
+        self.pressure = np.zeros(gsize, dtype=float)
+        self.momentum = np.zeros(gsize, dtype=float)
+
+        super().resize(size)
+
+    def on_click(self, mouse_event: MouseEvent):
+        if not self.collides_coords(mouse_event.position):
+            return False
+
+        if mouse_event.button is not MouseButton.NO_BUTTON:
+            y, x = self.absolute_to_relative_coords(mouse_event.position)
+
+            Y = 2 * y + 2
+
+            self.pressure[Y: Y + 2, x + 2: x + 4] += 2.0
+
+            return True
+
+    def on_press(self, key_press_event):
+        if key_press_event.key in ("r", "R"):
+            self.resize(self.size)  # Reset
+            return True
+
+    def render(self, canvas_view, colors_view, rect):
+        pressure = self.pressure
+        momentum = self.momentum
+
+        wrap_border(momentum)
+        wrap_border(pressure)
+
+        self.momentum = (
+            filter2D(momentum, -1, DIFFUSION, borderType=cv2.BORDER_REFLECT)
+            - VISCOSITY * momentum * filter2D(momentum, -1, CONVECTION, borderType=cv2.BORDER_REFLECT)
+        ) + filter2D(pressure, -1, CONVECTION, borderType=cv2.BORDER_REFLECT) * RHO
+
+        delta = filter2D(self.momentum, -1, POISSON, borderType=cv2.BORDER_REFLECT)
+
+        self.pressure = (
+            filter2D(pressure, -1, POISSON, borderType=cv2.BORDER_REFLECT)
+            + RHO / 2 * (delta - delta**2)
+        )
+
+        self.texture[..., :3] = (np.tanh(self.pressure[2: -2, 2: -2, None]) * WATER_COLOR).astype(int)
+
+        super().render(canvas_view, colors_view, rect)
+
+
+class FluidApp(App):
+    async def on_start(self):
+        self.root.add_widget(Fluid())
+
+
+FluidApp().run()
