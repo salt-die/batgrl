47761745e54ac2bf196c2a47fb98d0b468cbe6bd
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-13 23:24:42 -0600

    `_density_pressure`, `_forces`, `_integrate` methods combined into `step`. Bare working implementation.

--- examples/fluid/fluid/sph.py
+++ examples/fluid/fluid/sph.py
@@ -1,45 +1,19 @@
 import numpy as np
+from numpy import pi
 from scipy.spatial import KDTree
 
 from nurses_2.data_structures import Size
 
 
 class SPHSolver:
-    GRAVITY = .007
-    REST_DENS = 300.0
-    GAS_CONST = 2000.0
-    H = 16.0
-    MASS = 2.5
-    VISC = 200.0
-    POLY6 = 4.0 / (np.pi * H**8.0)
-    SPIKY_GRAD = -10.0 / (np.pi * H**5.0)
-    VISC_LAP = 40.0 / (np.pi * H**5.0)
-    BOUND_DAMPING = -.5
-
     def __init__(self, size: Size, nparticles=1000):
+        self.nparticles = nparticles
+        self.resize(size)
+
+    def resize(self, size: Size):
         self.size = size
-        self.state = np.zeros((nparticles, 8))
+        self.state = np.zeros((self.nparticles, 8), dtype=float)
         self.init_dam()
-
-    @property
-    def positions(self):
-        return self.state[:, :2]
-
-    @property
-    def velocities(self):
-        return self.state[:, 2:4]
-
-    @property
-    def acceleration(self):
-        return self.state[:, 4:6]
-
-    @property
-    def densities(self):
-        return self.state[:, 6]
-
-    @property
-    def pressure(self):
-        return self.state[:, 7]
 
     def init_dam(self):
         """
@@ -48,83 +22,96 @@ class SPHSolver:
         height, width = self.size
         dam_width = width / 5
 
-        self.positions[:] = np.random.random((self.state.shape[0], 2))
-        self.positions *= height, dam_width
-        self.positions[:, 1] += dam_width
+        positions = self.state[:, :2]
+
+        positions[:] = np.random.random((self.nparticles, 2))
+        positions[:] *= height, dam_width
+        positions[:, 1] += dam_width
 
     def step(self):
         """
         For each particle, compute densities and pressures, then forces, and
         finally integrate to obtain new positions.
         """
-        pairs = KDTree(self.positions).query_pairs(self.H)
-        self._density_pressure(pairs)
-        self._forces(pairs)
-        self._integrate()
+        H = 1.0
+        MASS = 2.5
+        GAS_CONST = 2000.0
+        REST_DENS = 300.0
+        VISC = 200.0
+        POLY6 = 4.0 / (np.pi * H**8.0)
+        SPIKY_GRAD = -10.0 / (np.pi * H**5.0)
+        VISC_LAP = 40.0 / (np.pi * H**5.0)
+        GRAVITY = 10.0
+        DT = .0001
 
-    def _density_pressure(self, pairs):
-        MASS = self.MASS
-        POLY6 = self.POLY6
-        H = self.H
-
-        positions = self.positions
-        densities = self.densities
-
-        norm = np.linalg.norm
-
-        densities[:] = 0.0
-
-        for i, j in pairs:
-            density = MASS * POLY6 * (H - norm(positions[i] - positions[j]))**3
-            densities[i] += density
-            densities[j] += density
-
-        self.pressure[:] = self.GAS_CONST * (densities - self.REST_DENS)
-
-    def _forces(self, pairs):
-        positions = self.positions
-        velocities = self.velocities
-        acceleration = self.acceleration
-        density = self.densities
-        pressure = self.pressure
-
-        MASS = self.MASS
-        SPIKY_GRAD = self.SPIKY_GRAD
-        H = self.H
-        VISC = self.VISC
-        VISC_LAP = self.VISC_LAP
+        state = self.state
+        positions = state[:, :2]
+        velocities = state[:, 2:4]
+        acceleration = state[:, 4:6]
+        densities = state[:, 6]
+        pressure = state[:, 7]
 
         norm = np.linalg.norm
 
-        acceleration[:] = 0.0
+        pairs = KDTree(positions).query_pairs(H)
 
-        for i, j in pairs:
-            relative = positions[i] - positions[j]
-            distance = norm(relative)
-            normal = relative / distance
+        # Density / Pressure update #############################################
+        densities[:] = 0.0                                                      #
+                                                                                #
+        for i, j in pairs:                                                      #
+            density = MASS * POLY6 * (H - norm(positions[i] - positions[j]))**3 #
+            densities[i] += density                                             #
+            densities[j] += density                                             #
+                                                                                #
+        np.clip(densities, .1, None, out=densities)
+        pressure[:] = GAS_CONST * (densities - REST_DENS)                       #
+        #########################################################################
 
-            strength = H - distance
+        # Forces update #############################################################################
+        acceleration[:] = 0.0                                                                       #
+                                                                                                    #
+        for i, j in pairs:                                                                          #
+            relative = positions[i] - positions[j]                                                  #
+            distance = norm(relative)                                                               #
+                                                                                                    #
+            normal = relative / distance                                                            #
+                                                                                                    #
+            strength = H - distance                                                                 #
+                                                                                                    #
+            force_ij = -normal * MASS * (pressure[i] + pressure[j]) * SPIKY_GRAD * strength**3 * .5 #
+            visc_ij = VISC * MASS * (velocities[j] - velocities[i]) * VISC_LAP * strength           #
+                                                                                                    #
+            acceleration[i] += force_ij / densities[j]                                              #
+            acceleration[i] += visc_ij / densities[j]                                               #
+                                                                                                    #
+            acceleration[j] += -force_ij / densities[i]                                             #
+            acceleration[j] += -visc_ij / densities[i]                                              #
+                                                                                                    #
+        acceleration[:, 0] += GRAVITY * (MASS / densities)                                          #
+        #############################################################################################
 
-            force_ij = -normal * MASS * (pressure[i] + pressure[j]) * SPIKY_GRAD * strength**3 * .5
-            visc_ij = VISC * MASS * (velocities[j] - velocities[i]) * VISC_LAP * strength
+        # Integrate
+        velocities += DT * acceleration / densities[:, None]
+        positions += DT * velocities
 
-            acceleration[i] += force_ij / density[j]
-            acceleration[i] += visc_ij / density[j]
+        # Move out-of-bounds particles back in-bounds. ###
 
-            acceleration[j] += -force_ij / density[i]
-            acceleration[j] += -visc_ij / density[j]
+        # Too tired to properly vectorize...
+        h, w = self.size
+        top = positions[:, 0] < 0
+        left = positions[:, 1] < 0
+        bottom = positions[:, 0] >= h
+        right = positions[:, 1] >= w
 
-        acceleration += self.GRAVITY * MASS / density
+        positions[:, 0][top] -= 2 * positions[:, 0][top]
+        positions[:, 1][left] -= 2 * positions[:, 1][left]
+        positions[:, 0][bottom] -= 2 * (positions[:, 0][bottom] - h)
+        positions[:, 1][right] -= 2 * (positions[:, 1][right] - w)
 
-    def _integrate(self):
-        velocities = self.velocities
-        positions = self.positions
+        velocities[:, 0][top] *= -.5
+        velocities[:, 1][left] *= -.5
+        velocities[:, 0][bottom] *= -.5
+        velocities[:, 1][right] *= -.5
+        ##################################################
 
-        velocities += self.acceleration / self.densities
-        positions += self.velocities
-
-        # Move out-of-bounds particles back in-bounds.
-        oob = (positions < 0) | (positions > self.size)
-
-        positions[oob] *= -1
-        velocities[oob] *= self.BOUND_DAMPING
+        np.clip(velocities, -1000, 1000, out=velocities) # Try to keep simulation from blowing up.
