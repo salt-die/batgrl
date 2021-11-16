f1304bb21696284dbdac73abeb97d9837da78f99
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-16 00:02:31 -0600

    Sliders added.

--- examples/fluid/fluid/__main__.py
+++ examples/fluid/fluid/__main__.py
@@ -1,16 +1,19 @@
 import numpy as np
 
 from nurses_2.app import App
-from nurses_2.colors import Color
+from nurses_2.colors import Color, color_pair, BLACK, BLACK_ON_BLACK
+from nurses_2.widgets import Widget
 from nurses_2.widgets.behaviors.auto_position_behavior import AutoPositionBehavior, Anchor
 from nurses_2.widgets.graphic_widget import GraphicWidget
+from nurses_2.widgets.slider import Slider
 
 from .sph import SPHSolver
 
 WATER_COLOR = Color.from_hex("1e1ea8")
+WATER_ON_BLACK = color_pair(WATER_COLOR, BLACK)
 
 
-class Fluid(AutoPositionBehavior, GraphicWidget):
+class Fluid(GraphicWidget):
     def __init__(self, *args, nparticles=1000, **kwargs):
         super().__init__(*args, **kwargs)
         y, x = self.size
@@ -41,15 +44,120 @@ class Fluid(AutoPositionBehavior, GraphicWidget):
         return super().render(canvas_view, colors_view, rect)
 
 
+class AutoPositionWidget(AutoPositionBehavior, Widget):
+    ...
+
+
 class MyApp(App):
     async def on_start(self):
-        self.root.add_widget(
-            Fluid(
-                size=(20, 50),
-                pos_hint=(.5, .5),
-                anchor=Anchor.CENTER,
-            )
+        WIDTH = 51
+        HWIDTH = WIDTH // 2
+        container = AutoPositionWidget(
+            size=(26, WIDTH),
+            pos_hint=(.5, .5),
+            anchor=Anchor.CENTER,
+            default_color_pair=BLACK_ON_BLACK
         )
+        container.colors[:6, :, :3] = 255
+
+        fluid = Fluid(pos=(6, 0), size=(20, 50))
+        solver = fluid.sph_solver
+
+        adjust_H = Slider(
+            width=HWIDTH,
+            pos=(1, 0),
+            min=.4,
+            max=1.44,
+            proportion=.04711,
+            handle_color=WATER_COLOR,
+            callback=lambda value: (
+                setattr(solver, "H", value),
+                container.add_text(f'{f"H: {solver.H}":<{HWIDTH}}'[:HWIDTH]),
+            ),
+            default_color_pair=WATER_ON_BLACK,
+        )
+
+        adjust_GAS_CONST = Slider(
+            width=HWIDTH,
+            pos=(1, HWIDTH + 1),
+            min=100.0,
+            max=4000.0,
+            proportion=.02564,
+            handle_color=WATER_COLOR,
+            callback=lambda value: (
+                setattr(solver, "GAS_CONST", value),
+                container.add_text(f'{f"GAS_CONST: {solver.GAS_CONST}":<{HWIDTH}}'[:HWIDTH], column=HWIDTH),
+            ),
+            default_color_pair=WATER_ON_BLACK,
+        )
+
+        adjust_REST_DENS = Slider(
+            width=HWIDTH,
+            pos=(3, 0),
+            min=40.0,
+            max=400.0,
+            proportion=.44444,
+            handle_color=WATER_COLOR,
+            callback=lambda value: (
+                setattr(solver, "REST_DENS", value),
+                container.add_text(f'{f"REST_DENS: {solver.REST_DENS}":<{HWIDTH}}'[:HWIDTH], row=2),
+            ),
+            default_color_pair=WATER_ON_BLACK,
+        )
+
+        adjust_POLYF = Slider(
+            width=HWIDTH,
+            pos=(3, HWIDTH + 1),
+            min=1.0,
+            max=10.0,
+            proportion=0.0,
+            handle_color=WATER_COLOR,
+            callback=lambda value: (
+                setattr(solver, "POLYF", value),
+                container.add_text(f'{f"POLY: {solver.POLYF}":<{HWIDTH}}'[:HWIDTH], row=2, column=HWIDTH),
+            ),
+            default_color_pair=WATER_ON_BLACK,
+        )
+
+        adjust_VISCF = Slider(
+            width=HWIDTH,
+            pos=(5, 0),
+            min=1000.0,
+            max=5000.0,
+            proportion=1.0,
+            handle_color=WATER_COLOR,
+            callback=lambda value: (
+                setattr(solver, "VISCF", value),
+                container.add_text(f'{f"VISC: {solver.VISCF}":<{HWIDTH}}'[:HWIDTH], row=4),
+            ),
+            default_color_pair=WATER_ON_BLACK,
+        )
+
+        adjust_SPIKYF = Slider(
+            width=HWIDTH,
+            pos=(5, HWIDTH + 1),
+            min=-10.0,
+            max=-1.0,
+            proportion=.55555,
+            handle_color=WATER_COLOR,
+            callback=lambda value: (
+                setattr(solver, "SPIKYF", value),
+                container.add_text(f'{f"SPIKY: {solver.SPIKYF}":<{HWIDTH}}'[:HWIDTH], row=4, column=HWIDTH),
+            ),
+            default_color_pair=WATER_ON_BLACK,
+        )
+
+        container.add_widgets(
+            fluid,
+            adjust_H,
+            adjust_GAS_CONST,
+            adjust_REST_DENS,
+            adjust_POLYF,
+            adjust_VISCF,
+            adjust_SPIKYF,
+        )
+
+        self.root.add_widget(container)
 
 
 MyApp().run()
--- examples/fluid/fluid/sph.py
+++ examples/fluid/fluid/sph.py
@@ -1,14 +1,6 @@
 import numpy as np
 from scipy.spatial import KDTree
 
-H = .49
-GAS_CONST = 200.0
-REST_DENS = 200.0
-POLY6 = 1.0 / (np.pi * H**8.0)
-VISC = 5000.0 / (np.pi * H**5.0)
-SPIKY_GRAD = -5.0 / (np.pi * H**5.0)
-GRAVITY = 40.0
-
 # ? A mysterious negative velocity bias is keeping Solver from settling.
 
 
@@ -16,6 +8,52 @@ class SPHSolver:
     def __init__(self, size, nparticles=1000):
         self.nparticles = nparticles
         self.resize(size)
+
+        self._H = .49
+        self.GAS_CONST = 200.0
+        self.REST_DENS = 200.0
+        self.POLYF = 1.0
+        self.VISCF = 5000.0
+        self.SPIKYF = -5.0
+        self.GRAVITY = 40.0
+
+    @property
+    def H(self):
+        return self._H
+
+    @H.setter
+    def H(self, value):
+        self._H = value
+        self.POLY6 = self._POLYF / (np.pi * value**8.0)
+        self.VISC = self._VISCF / (np.pi * value**5.0)
+        self.SPIKY_GRAD = self._SPIKYF / (np.pi * value**5.0)
+
+    @property
+    def POLYF(self):
+        return self._POLYF
+
+    @POLYF.setter
+    def POLYF(self, value):
+        self._POLYF = value
+        self.POLY6 = value / (np.pi * self.H**8.0)
+
+    @property
+    def VISCF(self):
+        return self._VISCF
+
+    @VISCF.setter
+    def VISCF(self, value):
+        self._VISCF = value
+        self.VISC = value / (np.pi * self.H**5.0)
+
+    @property
+    def SPIKYF(self):
+        return self._SPIKYF
+
+    @SPIKYF.setter
+    def SPIKYF(self, value):
+        self._SPIKYF = value
+        self.SPIKY_GRAD = value / (np.pi * self.H**5.0)
 
     def resize(self, size):
         self.size = size
@@ -40,6 +78,9 @@ class SPHSolver:
         For each particle, compute densities and pressures, then forces, and
         finally integrate to obtain new positions.
         """
+        H = self.H
+        REST_DENS = self.REST_DENS
+
         state = self.state
         positions    = state[:, :2]
         velocities   = state[:, 2:4]
@@ -56,17 +97,17 @@ class SPHSolver:
         strengths_cubed = strengths ** 3
 
         # Update density / pressure
-        density = POLY6 * strengths_cubed
+        density = self.POLY6 * strengths_cubed
 
         densities[:] = REST_DENS
         densities[js] += density
         densities[ks] += density
 
-        pressure[:] = GAS_CONST * (densities - REST_DENS)
+        pressure[:] = self.GAS_CONST * (densities - REST_DENS)
 
         # Update forces
         pressure_jk = (
-            SPIKY_GRAD
+            self.SPIKY_GRAD
             * relatives
             / distances[:, None]
             * (pressure[ks] + pressure[js])[:, None]
@@ -74,14 +115,14 @@ class SPHSolver:
         )
 
         viscs_jk = (
-            VISC
+            self.VISC
             * (velocities[ks] - velocities[js])
             * strengths[:, None]
          )
 
         total = pressure_jk + viscs_jk
 
-        acceleration[:, 0] += GRAVITY
+        acceleration[:, 0] += self.GRAVITY
         acceleration[js] += total
         acceleration[ks] -= total
         acceleration /= densities[:, None]
