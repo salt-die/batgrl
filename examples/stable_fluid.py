c7dd3de6b62225555369fbbd79eca7df160894fa
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-20 17:07:02 -0600

    Curl calculation added.

--- examples/stable_fluid.py
+++ examples/stable_fluid.py
@@ -21,15 +21,27 @@ class StableFluid(GraphicWidget):
         self.dye = np.zeros((h, w))
         self.indices = np.indices((h, w))
         self.velocity = np.zeros((2, h, w))
+        self.curl_mask = np.triu(np.ones((2, 2), dtype=bool), k=1)
 
     def render(self, canvas_view, colors_view, rect: Rect):
-        velocity = self.velocity
+        h, w = self.texture.shape
+        vy, vx = velocity = self.velocity
         dye = self.dye
+        curl_mask = self.curl_mask
 
         advection = self.indices - velocity
 
-        map_coordinates(advection, velocity[0], output=velocity[0], mode="wrap")
-        map_coordinates(advection, velocity[1], output=velocity[1], mode="wrap")
+        map_coordinates(advection, vy, output=vy, mode="wrap")
+        map_coordinates(advection, vx, output=vx, mode="wrap")
         map_coordinates(advection, dye, output=dye, mode="wrap")
+
+        # Reduce checkboard divergence
+        gaussian_filter(velocity, 1, output=velocity)
+        gaussian_filter(dye, 1, output=dye)
+
+        jacobian = np.stack((np.gradient(vy), np.gradient(vx))).reshape(2, 2, h, w)
+
+        divergence = jacobian.trace()
+        curl = (jacobian[curl_mask] - jacobian[curl_mask.T]).squeeze()
 
         super().render(canvas_view, colors_view, rect)
