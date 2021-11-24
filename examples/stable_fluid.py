e78376f0601d53260e036c28d61cd1ea41a1c4e0
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-24 16:36:48 -0600

    Working app. Fluid solver still has bugs.

--- examples/stable_fluid.py
+++ examples/stable_fluid.py
@@ -10,7 +10,9 @@ from scipy.ndimage.filters import convolve
 
 from nurses_2.colors import rainbow_gradient
 from nurses_2.io import MouseEvent, MouseButton
+from nurses_2.widgets.behaviors import AutoSizeBehavior
 from nurses_2.widgets.graphic_widget import GraphicWidget
+from nurses_2.app import App
 
 DIF_KERNEL = np.array([-.5, 0.0, .5])
 GRAD_KERNEL = np.array([-1.0, 0.0, 1.0])
@@ -25,24 +27,19 @@ POKE_RADIUS = 2.0
 RAINBOW_COLORS = cycle(rainbow_gradient(100))
 
 
-class StableFluid(GraphicWidget):
+class StableFluid(AutoSizeBehavior, GraphicWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.resize(self.size)
 
-    def resize(self, size: Size):
+    def resize(self, size):
         super().resize(size)
 
-        h, w = self.texture.shape
+        h, w, _ = self.texture.shape
 
-        self.dye = np.zeros((3, h, w))
+        self.dye = np.zeros((3, h, w), dtype=np.uint8)
         self.indices = np.indices((h, w))
         self.velocity = np.zeros((2, h, w))
-
-    def _init_inflow(self):
-        """
-        Init constant velocity field.
-        """
 
     def on_click(self, mouse_event: MouseEvent):
         """
@@ -57,7 +54,26 @@ class StableFluid(GraphicWidget):
         y, x = self.absolute_to_relative_coords(mouse_event.position)
         y *= 2
 
-    def render(self, canvas_view, colors_view, rect: Rect):
+        ys, xs = self.indices
+
+        poke_force = np.e**(-((ys - y)**2 + (xs - x)**2) / POKE_RADIUS)
+        self.velocity += poke_force
+
+        self.dye += np.moveaxis(
+            (poke_force[..., None] * next(RAINBOW_COLORS)).astype(np.uint8),
+            -1, 0,
+        )
+
+    def on_press(self, key_press_event):
+        match key_press_event.key:
+            case "r" | "R":
+                self.resize(self.size)  # Reset
+            case _:
+                return False
+
+        return True
+
+    def render(self, canvas_view, colors_view, rect):
         vy, vx = velocity = self.velocity
         dye = self.dye
 
@@ -88,15 +104,26 @@ class StableFluid(GraphicWidget):
         ########
         advection = self.indices - velocity
 
-        map_coordinates(advection, vy, output=vy, mode="wrap")
-        map_coordinates(advection, vx, output=vx, mode="wrap")
+        map_coordinates(vy, advection, output=vy, mode="wrap")
+        map_coordinates(vx, advection, output=vx, mode="wrap")
 
-        map_coordinates(advection, dye[0], output=dye[0], mode="wrap")
-        map_coordinates(advection, dye[1], output=dye[1], mode="wrap")
-        map_coordinates(advection, dye[2], output=dye[2], mode="wrap")
+        map_coordinates(dye[0], advection, output=dye[0], mode="wrap")
+        map_coordinates(dye[1], advection, output=dye[1], mode="wrap")
+        map_coordinates(dye[2], advection, output=dye[2], mode="wrap")
 
         # Reduce checkboard divergence
-        gaussian_filter(velocity, 1, output=velocity)
-        gaussian_filter(dye, 1, output=dye)
+        gaussian_filter(velocity[0], 1, output=velocity[0])
+        gaussian_filter(velocity[1], 1, output=velocity[1])
+        # gaussian_filter(dye, 1, output=dye)
+
+        self.texture[..., :3] = np.moveaxis(dye, 0, -1)
 
         super().render(canvas_view, colors_view, rect)
+
+
+class StableFluidApp(App):
+    async def on_start(self):
+        self.root.add_widget(StableFluid())
+
+
+StableFluidApp().run()
