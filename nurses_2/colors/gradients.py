d38389e95ee8f75cf7d24a33a587c42f9ec6f45e
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-11 02:22:24 -0600

    `rainbow_gradient` returns a list instead of a generator to be consistent with other gradient functions.
    `gradient` infers color type from first argument.

--- nurses_2/colors/gradients.py
+++ nurses_2/colors/gradients.py
@@ -13,7 +13,7 @@ __all__ = (
     "gradient",
 )
 
-def rainbow_gradient(n):
+def rainbow_gradient(n, *, color_type=Color):
     """
     Return a rainbow gradient of `n` `Color`s.
     """
@@ -21,8 +21,12 @@ def rainbow_gradient(n):
     OFFSETS = np.array([0, TAU / 3, 2 * TAU / 3])
     THETA = TAU / n
 
-    for i in range(n):
-        yield Color(*(int(i) for i in (np.sin(THETA * i + OFFSETS) * 127 + 128)))
+    return [
+        color_type(
+            *(int(j) for j in (np.sin(THETA * i + OFFSETS) * 127 + 128))
+        )
+        for i in range(n)
+    ]
 
 def foreground_rainbow(ncolors=20, bg_color: Color=BLACK):
     """
@@ -53,14 +57,17 @@ def gradient(start, end, ncolors):
     """
     Return a gradient from `start` to `end` with `ncolors` (> 1) colors.
     """
-    assert ncolors > 1, f"not enough colors ({ncolors=})"
+    if ncolors < 1:
+        raise ValueError(f"not enough colors ({ncolors=})")
+
+    color_type = type(start)
 
     grad = [ start ]
 
     for i in range(ncolors - 2):
         proportion = (i + 1) / (ncolors - 1)
         grad.append(
-            tuple(lerp(start, end, proportion))
+            color_type(*lerp(start, end, proportion))
         )
 
     grad.append(end)
