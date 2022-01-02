59338a801fc46573a293e42f842b35a5f6646af1
salt-die <53280662+salt-die@users.noreply.github.com>
2022-01-02 10:45:52 -0600

    7-segment display upgraded to 14-segment.

--- examples/digital_clock.py
+++ examples/digital_clock.py
@@ -1,49 +1,61 @@
 import asyncio
 import time
-from itertools import chain
 
 from nurses_2.app import App
 from nurses_2.widgets.digital_display import DigitalDisplay, BRIGHT_GREEN_ON_BLACK
 from nurses_2.widgets.text_widget import TextWidget
 
-def _formatted_time(twelve_hour=False):
-    hours, minutes, seconds = time.localtime()[3: 6]
-
-    if twelve_hour:
-        hours %= 12
-
-    return chain.from_iterable(divmod(n, 10) for n in (hours, minutes, seconds))
-
 
 class DigitalClock(TextWidget):
     def __init__(self, pos=(0, 0), twelve_hour=False, default_color_pair=BRIGHT_GREEN_ON_BLACK, **kwargs):
         super().__init__(
-            size=(7, 47),
+            size=(7, 52),
             pos=pos,
             default_color_pair=default_color_pair,
             **kwargs,
         )
         self.twelve_hour = twelve_hour
 
-        offsets = (0, 0, 2, 2, 4, 4)
         for i in range(6):
-            self.add_widget(DigitalDisplay(pos=(0, i * 7 + offsets[i])))
+            self.add_widget(DigitalDisplay(pos=(0, i * 8 + i // 2 * 2)))
 
-        self.canvas[[2, -3], 14] = self.canvas[[2, -3], 30] = "●"
+        self.canvas[[2, -3], 16] = self.canvas[[2, -3], 34] = "●"
 
         self._update_task = asyncio.create_task(self._update_time())
 
+    def _formatted_time(self):
+        hours, minutes, seconds = time.localtime()[3: 6]
+
+        if self.twelve_hour:
+            hours %= 12
+
+        for unit in (hours, minutes, seconds):
+            for n in divmod(unit, 10):
+                yield n
+
     async def _update_time(self):
         while True:
-            for display, digit in zip(self.children, _formatted_time(self.twelve_hour)):
-                display.show_digit(digit)
+            for display, digit in zip(self.children, self._formatted_time()):
+                display.show_char(str(digit))
 
             await asyncio.sleep(1)
 
 
+class TestDisplay(DigitalDisplay):
+    """
+    Display any key pressed.
+    """
+    def on_press(self, key_press_event):
+        key = key_press_event.key
+        if isinstance(key, str) and len(key) == 1:
+            self.show_char(key)
+
+            return True
+
+
 class DigitalClockApp(App):
     async def on_start(self):
-        self.add_widget(DigitalClock(twelve_hour=True))
+        self.add_widgets(DigitalClock(twelve_hour=True), TestDisplay(pos=(10, 0)))
 
 
 DigitalClockApp().run()
--- nurses_2/widgets/digital_display.py
+++ nurses_2/widgets/digital_display.py
@@ -10,18 +10,103 @@ BRIGHT_GREEN = Color.from_hex("33e860")
 DIM_GREEN_ON_BLACK = ColorPair.from_colors(DIM_GREEN, BLACK)
 BRIGHT_GREEN_ON_BLACK = ColorPair.from_colors(BRIGHT_GREEN, BLACK)
 
-_DIGIT_TO_SEGMENTS = [
-    "abcefg",
-    "cf",
-    "acdeg",
-    "acdfg",
-    "bcdf",
-    "abdfg",
-    "abdefg",
-    "acf",
-    "abcdefg",
-    "abcdfg",
-]
+_CHAR_TO_SEGMENTS = {
+    " ": (),
+    "0": ("a", "b", "c", "d", "e", "f"),
+    "1": ("b", "c"),
+    "2": ("a", "b", "g2", "g1", "e", "d"),
+    "3": ("a", "b", "g2", "c", "d"),
+    "4": ("f", "b", "g1", "g2", "c"),
+    "5": ("a", "f", "g1", "m", "d"),
+    "6": ("a", "f", "g1", "g2", "e", "c", "d"),
+    "7": ("a", "b", "c"),
+    "8": ("a", "f", "b", "g1", "g2", "e", "c", "d"),
+    "9": ("a", "f", "b", "g1", "g2", "c", "d"),
+    "a": ("e", "g1", "l", "d"),
+    "b": ("f", "g1", "e", "m", "d"),
+    "c": ("g1", "g2", "e", "d"),
+    "d": ("k", "g2", "b", "c", "d"),
+    "e": ("g1", "e", "k", "d"),
+    "f": ("g1", "j", "g2", "l"),
+    "g": ("j", "b", "g2", "c", "d"),
+    "h": ("f", "g1", "e", "l"),
+    "i": ("l",),
+    "j": ("e", "k", "i"),
+    "k": ("i", "l", "j", "m"),
+    "l": ("f", "e"),
+    "m": ("e", "g1", "l", "g2", "c"),
+    "n": ("e", "g1", "l"),
+    "o": ("g1", "g2", "e", "c", "d"),
+    "p": ("f", "h", "g1", "e"),
+    "q": ("j", "b", "g2", "c"),
+    "r": ("e", "g1"),
+    "s": ("g2", "m", "d"),
+    "t": ("f", "g1", "e", "d"),
+    "u": ("e", "d", "c"),
+    "v": ("e", "k"),
+    "w": ("e", "k", "m", "c"),
+    "x": ("h", "j", "k", "m"),
+    "y": ("i", "g2", "b", "c", "d"),
+    "z": ("g1", "k", "d"),
+    "A": ("a", "f", "b", "g1", "g2", "e", "c"),
+    "B": ("a", "i", "b", "g2", "l", "c", "d"),
+    "C": ("a", "f", "e", "d"),
+    "D": ("a", "i", "b", "l", "c", "d"),
+    "E": ("a", "f", "g1", "e", "d"),
+    "F": ("a", "f", "g1", "e"),
+    "G": ("a", "f", "g2", "e", "c", "d"),
+    "H": ("f", "b", "g1", "g2", "e", "c"),
+    "I": ("a", "i", "l", "d"),
+    "J": ("e", "d", "c", "b"),
+    "K": ("f", "g1", "j", "e", "m"),
+    "L": ("f", "e", "d"),
+    "M": ("f", "h", "j", "b", "e", "c"),
+    "N": ("f", "h", "b", "e", "m", "c"),
+    "O": ("a", "f", "b", "e", "c", "d"),
+    "P": ("a", "f", "b", "g1", "g2", "e"),
+    "Q": ("a", "f", "b", "e", "m", "c", "d"),
+    "R": ("a", "f", "b", "g1", "g2", "e", "m"),
+    "S": ("a", "f", "g1", "g2", "c", "d"),
+    "T": ("a", "i", "l"),
+    "U": ("f", "b", "e", "c", "d"),
+    "V": ("f", "e", "k", "j"),
+    "W": ("f", "b", "e", "k", "m", "c"),
+    "X": ("h", "j", "k", "m"),
+    "Y": ("f", "b", "g1", "g2", "c", "d"),
+    "Z": ("a", "j", "k", "d"),
+    "!": ("b", "c", "dp"),
+    '"': ("i", "b"),
+    "#": ("g1", "i", "g2", "b", "l", "c", "d"),
+    "$": ("a", "f", "i", "g1", "g2", "l", "c", "d"),
+    "%": ("f", "h", "j", "g1", "g2", "k", "m", "c"),
+    "&": ("a", "h", "i", "g1", "e", "m", "d", "c"),
+    "'": ("i",),
+    "(": ("j", "m"),
+    ")": ("h", "k"),
+    "*": ("h", "i", "j", "g1", "g2", "k", "l", "m"),
+    "+": ("i", "g1", "g2", "l"),
+    ",": ("k",),
+    "-": ("g1", "g2"),
+    ".": ("dp",),
+    "/": ("k", "j"),
+    ":": ("i", "l"),
+    ";": ("i", "k"),
+    "<": ("g1", "j", "m"),
+    "=": ("g1", "g2", "d"),
+    ">": ("h", "k", "g2"),
+    "?": ("a", "b", "g2", "l", "dp"),
+    "@": ("i", "g2", "b", "a", "f", "e", "d"),
+    "[": ("a", "f", "e", "d"),
+    "\\": ("h", "m"),
+    "]": ("a", "b", "c", "d"),
+    "^": ("k", "m"),
+    "_": ("d",),
+    "`": ("h",),
+    "{": ("a", "h", "g1", "k", "d"),
+    "|": ("i", "l"),
+    "}": ("a", "j", "g2", "m", "d"),
+    "~": ("g1", "j", "g2", "k"),
+}
 
 
 class Segment:
@@ -39,8 +124,8 @@ class Segment:
 
 
 class DigitalDisplay(TextWidget):
-    """
-    A 7x6 seven-segment display widget.
+    r"""
+    A 7x8 14-segment (plus decimal point) display widget.
 
     Parameters
     ----------
@@ -49,30 +134,39 @@ class DigitalDisplay(TextWidget):
     on_color_pair : ColorPair, default: BRIGHT_GREEN_ON_BLACK
         Color pair of on segments.
 
-    Use `show_digit` method to display a specific digit or light/dim
-    individual segments by setting a-g to True or False, e.g.,
+    Use `show_char` method to display a ascii character or turn on/off
+    individual segments by setting one of ("a", "b", "c", "d", "e",
+    "f", "g1", "g2", "h", "i", "j", "k", "l", "m", "dp") to True or False, e.g.,
     `digital_display.f = True`. The segments are labeled according
     to the following diagram:
 
     ```
         a
-      ━━━━
-    b┃    ┃c
-     ┃  d ┃
-      ━━━━
-    e┃    ┃f
-     ┃    ┃
-      ━━━━
-        g
+      ━━━━━
+    f┃  ┃i ┃b     h \   / j
+     ┃  ┃  ┃         \ /
+                 g1 ━━ ━━ g2
+    e┃  ┃l ┃c        / \
+     ┃  ┃  ┃      k /   \ m
+      ━━━━━ ● dp
+        d
     ```
     """
-    a = Segment(np.s_[ 0, 1: -1])
-    b = Segment(np.s_[ 1: 3,  0])
-    c = Segment(np.s_[ 1: 3, -1])
-    d = Segment(np.s_[ 3, 1: -1])
-    e = Segment(np.s_[ 4: 6,  0])
-    f = Segment(np.s_[ 4: 6, -1])
-    g = Segment(np.s_[-1, 1: -1])
+    a  = Segment(np.s_[0, 1: 6])
+    b  = Segment(np.s_[1: 3, 6])
+    c  = Segment(np.s_[4: 6, 6])
+    d  = Segment(np.s_[6, 1: 6])
+    e  = Segment(np.s_[4: 6,  0])
+    f  = Segment(np.s_[1: 3,  0])
+    g1 = Segment(np.s_[3, 1: 3])
+    g2 = Segment(np.s_[3, 4: 6])
+    h  = Segment(np.s_[(1, 2), (1, 2)])
+    i  = Segment(np.s_[1: 3, 3])
+    j  = Segment(np.s_[(1, 2), (5, 4)])
+    k  = Segment(np.s_[(4, 5), (2, 1)])
+    l  = Segment(np.s_[4: 6, 3])
+    m  = Segment(np.s_[(4, 5), (4, 5)])
+    dp = Segment(np.s_[6, 7:8])
 
     def __init__(
         self,
@@ -84,23 +178,29 @@ class DigitalDisplay(TextWidget):
         kwargs.pop("size", None)
         kwargs.pop("size_hint", None)
 
-        super().__init__(size=Size(7, 6), **kwargs)
+        super().__init__(size=Size(7, 8), **kwargs)
 
-        self.colors[:] = self.off_color_pair = off_color_pair
+        self.off_color_pair = off_color_pair
         self.on_color_pair = on_color_pair
 
         canvas = self.canvas
-        canvas[[0, 3, 6], 1: -1] = "━"
-        canvas[1: 3,  [0, -1]] = canvas[4: 6, [0, -1]] = "┃"
+        canvas[[0, 6], 1: 6] = canvas[3, 1: 3] = canvas[3, 4: 6] = "━"
+        canvas[1: 3,  [0, 3, 6]] = canvas[4: 6, [0, 3, 6]] = "┃"
+        canvas[(1, 2, 4, 5), (1, 2, 4, 5)] = "\\"
+        canvas[(1, 2, 4, 5), (5, 4, 2, 1)] = "/"
+        canvas[6, 7] = "●"
+
+        self._where_segments = canvas != self.default_char
+        self.colors[self._where_segments] = off_color_pair
 
     def resize(self, size: Size):
         pass
 
-    def show_digit(self, digit: int):
-        if digit not in range(10):
-            raise ValueError(f"not a digit")
+    def show_char(self, char: str):
+        if not isinstance(char, str) or len(char) != 1:
+            raise ValueError(f"{char} is not a single ascii character")
 
-        self.colors[:] = self.off_color_pair
+        self.colors[self._where_segments] = self.off_color_pair
 
-        for segment in _DIGIT_TO_SEGMENTS[digit]:
+        for segment in _CHAR_TO_SEGMENTS[char]:
             setattr(self, segment, True)
