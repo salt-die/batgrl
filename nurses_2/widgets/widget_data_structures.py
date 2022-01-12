0789152120d6418524476a9cbab5f02f43ccbb55
salt-die <53280662+salt-die@users.noreply.github.com>
2022-01-12 11:50:11 -0600

    Transition enum added.

--- nurses_2/widgets/widget_data_structures.py
+++ nurses_2/widgets/widget_data_structures.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import NamedTuple
 
-__all__ = "SizeHint", "PosHint", "Anchor"
+__all__ = "SizeHint", "PosHint", "Anchor", "Transition"
 
 
 class SizeHint(NamedTuple):
@@ -24,3 +24,37 @@ class Anchor(str, Enum):
     BOTTOM_LEFT = "BOTTOM_LEFT"
     BOTTOM_CENTER = "BOTTOM_CENTER"
     BOTTOM_RIGHT = "BOTTOM_RIGHT"
+
+
+class Transition(str, Enum):
+    LINEAR = "linear"
+    IN_QUAD = "in_quad"
+    OUT_QUAD = "out_quad"
+    IN_OUT_QUAD = "in_out_quad"
+    IN_CUBIC = "in_cubic"
+    OUT_CUBIC = "out_cubic"
+    IN_OUT_CUBIC = "in_out_cubic"
+    IN_QUART = "in_quart"
+    OUT_QUART = "out_quart"
+    IN_OUT_QUART = "in_out_quart"
+    IN_QUINT = "in_quint"
+    OUT_QUINT = "out_quint"
+    IN_OUT_QUINT = "in_out_quint"
+    IN_SINE = "in_sine"
+    OUT_SINE = "out_sine"
+    IN_OUT_SINE = "in_out_sine"
+    IN_EXP = "in_exp"
+    OUT_EXP = "out_exp"
+    IN_OUT_EXP = "in_out_exp"
+    IN_CIRC = "in_circ"
+    OUT_CIRC = "out_circ"
+    IN_OUT_CIRC = "in_out_circ"
+    IN_ELASTIC = "in_elastic"
+    OUT_ELASTIC = "out_elastic"
+    IN_OUT_ELASTIC = "in_out_elastic"
+    IN_BACK = "in_back"
+    OUT_BACK = "out_back"
+    IN_OUT_BACK = "in_out_back"
+    IN_BOUNCE = "in_bounce"
+    OUT_BOUNCE = "out_bounce"
+    IN_OUT_BOUNCE = "in_out_bounce"
