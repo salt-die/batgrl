8f5cb98ba1d244ab2235654f05ddd02b7da47321
salt-die <53280662+salt-die@users.noreply.github.com>
2022-01-12 18:20:55 -0600

    Example iterates over all transitions.

--- examples/transitions.py
+++ examples/transitions.py
@@ -1,32 +1,42 @@
 from pathlib import Path
+from itertools import cycle
 
 from nurses_2.app import App
 from nurses_2.widgets.image import Image
+from nurses_2.widgets.text_widget import TextWidget, Anchor
 from nurses_2.widgets.widget_data_structures import Transition
 
 IMAGE_DIR = Path("images")
 PATH_TO_LOGO = IMAGE_DIR / "logo_solo_flat_256.png"
 
+ALPHAS = cycle((0.0, 1.0))
+POS_HINTS = cycle(((0.0, 0.0), (.5, .5)))
+SIZE_HINTS = cycle(((.25, .25), (.5, .5)))
+
 class MyApp(App):
     async def on_start(self):
-        rect = Image(path=PATH_TO_LOGO, size=(10, 20))
+        logo = Image(
+            path=PATH_TO_LOGO,
+            size_hint=next(SIZE_HINTS),
+            pos_hint=next(POS_HINTS),
+        )
 
-        self.add_widget(rect)
+        label = TextWidget(size=(1, 20), pos_hint=(None, .5), anchor=Anchor.TOP_CENTER)
 
-        await rect.transition(pos=(20, 75), alpha=0.0, duration=2, transition_type=Transition.IN_CUBIC)
+        self.add_widgets(logo, label)
 
-        await rect.transition(pos=(0, 0), alpha=1.0, duration=3, transition_type=Transition.OUT_BOUNCE)
+        for transition in Transition:
+            label.add_text(f"{transition:^20}")
 
-        # Hints must not be `None` or transition will error.
-        rect.size_hint = .1, .1
-        rect.y_hint = 0
+            await logo.transition(
+                pos_hint=next(POS_HINTS),
+                alpha=next(ALPHAS),
+                size_hint=next(SIZE_HINTS),
+                transition_type=transition,
+                duration=5.0,
+            )
 
-        await rect.transition(y_hint=1.0, size_hint=(1.0, 1.0), alpha=0.0, duration=4, transition_type=Transition.OUT_SINE)
-
-        rect.size_hint = None, None
-        rect.y_hint = None
-
-        await rect.transition(y=0, x=0, height=10, width=20, alpha=1, duration=5, transition_type=Transition.IN_OUT_EXP)
+        self.exit()
 
 
 MyApp().run()
