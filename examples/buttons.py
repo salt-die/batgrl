ce99aade696a54b5133def3ed79180a7db5a5722
salt-die <53280662+salt-die@users.noreply.github.com>
2022-01-14 02:54:32 -0600

    Callback function added.

--- examples/buttons.py
+++ examples/buttons.py
@@ -1,13 +1,17 @@
 from nurses_2.app import App
+from nurses_2.widgets.text_widget import TextWidget
 from nurses_2.widgets.button import Button
 
 
 class MyApp(App):
     async def on_start(self):
+        display = TextWidget(size=(1, 20), pos=(7, 15))
+        self.add_widget(display)
+
         self.add_widgets(
             Button(
                 label=f"Button {i + 1}",
-                callback=lambda: None,
+                callback=lambda text=f"Button {i + 1} pressed!": display.add_text(text),
                 size=(3, 10),
                 pos=(3 * i, 0),
             )
