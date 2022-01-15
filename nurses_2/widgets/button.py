d55207e0df9692383bd1d9968cbc841f72b0dfb7
salt-die <53280662+salt-die@users.noreply.github.com>
2022-01-15 14:28:19 -0600

    Button api change.

--- nurses_2/widgets/button.py
+++ nurses_2/widgets/button.py
@@ -1,14 +1,16 @@
 from typing import Callable
 
 from ..colors import Color, ColorPair, BLACK
+from ..easings import lerp
 from .behaviors.button_behavior import ButtonBehavior
-from .text_widget import TextWidget, Anchor
+from .text_widget import TextWidget, Anchor, Size
 
 PURPLE = Color.from_hex("462270")
-LIGHT_PURPLE = Color.from_hex("592c8c")
-LIGHTER_PURPLE = Color.from_hex("753ab7")
 YELLOW = Color.from_hex("dbd006")
 YELLOW_ON_PURPLE = ColorPair.from_colors(YELLOW, PURPLE)
+
+def _whiten_color_pair(color_pair, p):
+    return ColorPair(*(int(lerp(c, 255, p)) for c in color_pair))
 
 
 class Button(ButtonBehavior, TextWidget):
@@ -21,66 +23,91 @@ class Button(ButtonBehavior, TextWidget):
         Button label.
     callback : Callable, default: lambda: None
         No-argument callable called when button is released.
-    hover_color : Color | None, default: LIGHT_PURPLE
-        Color of button on hover. If None, hover_color
-        will be default_bg_color lightened.
-    pressed_color : Color, default: LIGHTER_PURPLE
-        Color of button when pressed.
+    hover_color_pair : ColorPair | None, default: None
+        Color pair of button on hover. If None, hover_color_pair
+        will be default_color_pair, but whitened.
+    down_color_pair : ColorPair | None, default: None
+        Color pair of button when pressed. If none, down_color_pair
+        will be default_color_pair, but whitened (and brighter than
+        default hover_color_pair).
     background_color : Color, default: BLACK
-        Color of background for the button. Looks best with
-        button's parent's background color.
+        Color of background for the button.
+    default_color_pair: ColorPair, default: YELLOW_ON_PURPLE
+        Default color pair of button.
     """
     def __init__(
         self,
         *,
         label: str="",
         callback: Callable=lambda: None,
-        hover_color: Color | None=LIGHT_PURPLE,
-        pressed_color: Color=LIGHTER_PURPLE,
+        hover_color_pair: ColorPair | None=None,
+        down_color_pair: ColorPair | None=None,
         background_color: Color=BLACK,
         default_color_pair: ColorPair=YELLOW_ON_PURPLE,
         **kwargs,
     ):
-
-        self.label = TextWidget(
-            size=(1, len(label)),
+        self._label_widget = TextWidget(
+            size=(1, 1),
             pos=(1, 0),
             pos_hint=(None, .5),
             anchor=Anchor.TOP_CENTER,
             default_color_pair=default_color_pair,
         )
-        self.label.add_text(label)
+
+        self.callback = callback
+
+        if hover_color_pair is None:
+            self.hover_color_pair = _whiten_color_pair(default_color_pair, .1)
+        else:
+            self.hover_color_pair = hover_color_pair
+
+        if down_color_pair is None:
+            self.down_color_pair = _whiten_color_pair(default_color_pair, .25)
+        else:
+            self.down_color_pair = down_color_pair
+
+        self.background_color = background_color
 
         super().__init__(default_color_pair=default_color_pair, **kwargs)
 
-        self.add_widget(self.label)
+        self.add_widget(self._label_widget)
 
-        if hover_color is None:
-            self.hover_color = Color(
-                *(127 + c // 2 for c in self.default_bg_color)
-            )
-        else:
-            self.hover_color = hover_color
+        self.label = label
 
-        self.pressed_color = pressed_color
+        self.resize(self.size)
+
+    def resize(self, size: Size):
+        super().resize(size)
+
+        self.canvas[:] =  self.default_char
+        self.colors[:] = self.default_color_pair
 
         self.canvas[0] = "▀"
         self.canvas[-1] = "▄"
-        self.colors[0, :, :3] = self.colors[-1, :, :3] = background_color
+        self.colors[0, :, :3] = self.colors[-1, :, :3] = self.background_color
 
-        self.callback = callback
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @label.setter
+    def label(self, label: str):
+        self._label = label
+        self._label_widget.resize((1, len(label)))
+        self._label_widget.update_geometry()
+        self._label_widget.add_text(label)
 
     def on_release(self):
         self.callback()
 
     def update_hover(self):
-        self.colors[..., -3:] = self.hover_color
-        self.label.colors[..., -3:] = self.hover_color
+        self.colors[:] = self._label_widget.colors[:] = self.hover_color_pair
+        self.colors[0, :, :3] = self.colors[-1, :, :3] = self.background_color
 
     def update_down(self):
-        self.colors[..., -3:] = self.pressed_color
-        self.label.colors[..., -3:] = self.pressed_color
+        self.colors[:] = self._label_widget.colors[:] = self.down_color_pair
+        self.colors[0, :, :3] = self.colors[-1, :, :3] = self.background_color
 
     def update_normal(self):
-        self.colors[..., -3:] = self.default_bg_color
-        self.label.colors[..., -3:] = self.default_bg_color
+        self.colors[:] = self._label_widget.colors[:] = self.default_color_pair
+        self.colors[0, :, :3] = self.colors[-1, :, :3] = self.background_color
