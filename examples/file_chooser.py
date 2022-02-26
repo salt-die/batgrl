8358008989216d5193d720deaed5e13c685370b2
salt-die <53280662+salt-die@users.noreply.github.com>
2022-02-25 18:06:56 -0600

    Label added to see `select_callback` effect.

--- examples/file_chooser.py
+++ examples/file_chooser.py
@@ -1,4 +1,14 @@
-from nurses_2.app import run_widget_as_app
+from nurses_2.app import App
 from nurses_2.widgets.file_chooser import FileChooser
+from nurses_2.widgets.text_widget import TextWidget
 
-run_widget_as_app(FileChooser, size=(20, 25), size_hint=(1.0, None))
+
+class FileApp(App):
+    async def on_start(self):
+        label = TextWidget(size=(1, 50), pos=(0, 26))
+        select_callback = lambda path: label.add_text(f"{f'{path.name} selected!':<50}"[:50])
+
+        self.add_widgets(label, FileChooser(size=(20, 25), size_hint=(1.0, None), select_callback=select_callback))
+
+
+FileApp().run()
