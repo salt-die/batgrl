85260c8be27f1d85cd4b074fa386eaa375fa87fe
salt-die <53280662+salt-die@users.noreply.github.com>
2022-03-01 15:36:54 -0600

    Window transparency added to example.

--- examples/windows.py
+++ examples/windows.py
@@ -12,7 +12,7 @@ NIGHT_PATH = Path("frames") / "night"
 class MyApp(App):
     async def on_start(self):
         for path in (CAVEMAN_PATH, SPINNER_PATH, NIGHT_PATH):
-            window = Window(size=(25, 50), title=path.name)
+            window = Window(size=(25, 50), alpha=.7, title=path.name)
             animation = Animation(size_hint=(1.0, 1.0), path=path)
             window.add_widget(animation)
             animation.play()
