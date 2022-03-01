5c3e24233efd61e8ce7c01e1ba71ac80cc98444d
salt-die <53280662+salt-die@users.noreply.github.com>
2022-03-01 08:28:12 -0600

    Example renamed.

--- examples/windows.py
+++ examples/windows.py
@@ -3,21 +3,16 @@ from pathlib import Path
 from nurses_2.app import App
 from nurses_2.widgets.animation import Animation
 from nurses_2.widgets.window import Window
-from nurses_2.widgets.behaviors.focus_behavior import FocusBehavior
 
 CAVEMAN_PATH = Path("frames") / "caveman"
 SPINNER_PATH = Path("frames") / "spinner"
 NIGHT_PATH = Path("frames") / "night"
 
 
-class FocusableWindow(FocusBehavior, Window):
-    ...
-
-
 class MyApp(App):
     async def on_start(self):
         for path in (CAVEMAN_PATH, SPINNER_PATH, NIGHT_PATH):
-            window = FocusableWindow(size=(25, 50))
+            window = Window(size=(25, 50), title=path.name)
             animation = Animation(size_hint=(1.0, 1.0), path=path)
             window.add_widget(animation)
             animation.play()
