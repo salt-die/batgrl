7184926dc8992dfaa684388f7ceac9947b0beadd
salt-die <53280662+salt-die@users.noreply.github.com>
2022-03-08 11:41:08 -0600

    Update example to showcase different widgets.

--- examples/windows.py
+++ examples/windows.py
@@ -2,22 +2,27 @@ from pathlib import Path
 
 from nurses_2.app import App
 from nurses_2.widgets.animation import Animation
+from nurses_2.widgets.file_chooser import FileChooser
+from nurses_2.widgets.color_picker import ColorPicker
 from nurses_2.widgets.window import Window
 
 CAVEMAN_PATH = Path("frames") / "caveman"
-SPINNER_PATH = Path("frames") / "spinner"
-NIGHT_PATH = Path("frames") / "night"
 
 
 class MyApp(App):
     async def on_start(self):
-        for path in (CAVEMAN_PATH, SPINNER_PATH, NIGHT_PATH):
-            window = Window(size=(25, 50), alpha=.7, title=path.name)
-            animation = Animation(size_hint=(1.0, 1.0), path=path)
-            window.add_widget(animation)
-            animation.play()
+        animation = Animation(size_hint=(1.0, 1.0), path=CAVEMAN_PATH)
+        animation.play()
+        window_1 = Window(size=(25, 50), alpha=.7, title=CAVEMAN_PATH.name)
+        window_1.add_widget(animation)
 
-            self.add_widget(window)
+        window_2 = Window(size=(25, 50), alpha=.7, title="File Chooser")
+        window_2.add_widget(FileChooser(size_hint=(1.0, 1.0)))
+
+        window_3 = Window(size=(25, 50), alpha=.7, title="Color Picker")
+        window_3.add_widget(ColorPicker(size_hint=(1.0, 1.0)))
+
+        self.add_widgets(window_1, window_2, window_3)
 
 
 MyApp().run()
