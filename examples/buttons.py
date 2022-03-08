955fe7bc9f2c0eab1a435fc769371f9638d5bac3
salt-die <53280662+salt-die@users.noreply.github.com>
2022-03-08 02:52:03 -0600

    Added buttons to a `GridLayout`.

--- examples/buttons.py
+++ examples/buttons.py
@@ -1,14 +1,15 @@
 import asyncio
 
 from nurses_2.app import App
-from nurses_2.widgets.text_widget import TextWidget
 from nurses_2.widgets.button import Button
+from nurses_2.widgets.grid_layout import GridLayout
+from nurses_2.widgets.text_widget import TextWidget
 from nurses_2.widgets.toggle_button import ToggleButton, ToggleState
 
 
 class MyApp(App):
     async def on_start(self):
-        display = TextWidget(size=(1, 20), pos=(1, 15))
+        display = TextWidget(size=(1, 20), pos=(1, 9))
         display._clear_task = asyncio.create_task(asyncio.sleep(0))  # dummy task
 
         async def clear_display():
@@ -31,41 +32,34 @@ class MyApp(App):
 
             return callback
 
-        self.add_widget(display)
+        grid_layout = GridLayout(5, 3, pos=(2, 0), size=(5, 36))
 
         # Buttons
-        self.add_widgets(
-            Button(
-                label=f"Button {i + 1}",
-                callback=button_callback(i),
-                size=(1, 10),
-                pos=(2 * i, 0),
-            )
+        buttons = [
+            Button(label=f"Button {i + 1}", callback=button_callback(i))
             for i in range(5)
-        )
+        ]
 
         # Independent toggle buttons
-        self.add_widgets(
-            ToggleButton(
-                label=f"Button {i + 1}",
-                callback=toggle_button_callback(i),
-                size=(1, 12),
-                pos=(5 + i, 0),
-            )
+        toggles = [
+            ToggleButton(label=f"Button {i + 1}", callback=toggle_button_callback(i))
             for i in range(5, 10)
-        )
+        ]
 
         # Grouped radio buttons
-        self.add_widgets(
+        radios = [
             ToggleButton(
                 group="my_group",
                 label=f"Button {i + 1}",
                 callback=toggle_button_callback(i),
-                size=(1, 12),
-                pos=(6 + i, 0),
             )
             for i in range(10, 15)
-        )
+        ]
+
+        for button, toggle, radio in zip(buttons, toggles, radios):
+            grid_layout.add_widgets(button, toggle, radio)
+
+        self.add_widgets(display, grid_layout)
 
 
 MyApp().run()
