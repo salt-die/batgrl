44f004523ae38a557d73915b6dfedae011d84eef
salt-die <53280662+salt-die@users.noreply.github.com>
2022-06-21 12:57:00 -0500

    Rainbow snake!

--- examples/snake.py
+++ examples/snake.py
@@ -5,81 +5,63 @@ Move snake with arrow keys. Pause with `space`.
 """
 import asyncio
 from collections import deque
-from enum import Enum
-from itertools import product
+from itertools import product, cycle
 from pathlib import Path
 from random import choice
 
 from nurses_2.app import App
-from nurses_2.colors import AGREEN, ARED
+from nurses_2.colors import AColor, ARED, AWHITE, gradient, rainbow_gradient
 from nurses_2.widgets.graphic_widget import GraphicWidget
-from nurses_2.widgets.image import Image
+from nurses_2.widgets.animation import Animation
 
-PATH_TO_LOGO_FULL = Path("images") / "python_discord_logo.png"
+PATH_TO_PRIDE = Path("frames") / "pride"
 HEIGHT, WIDTH = 20, 20
 SNAKE_START = HEIGHT // 2, WIDTH // 2
 TICK_DURATION = .12
+RAINBOW_GRADIENT = rainbow_gradient(40, color_type=AColor)
+APPLE_GRADIENT = cycle((half := gradient(ARED, AWHITE, 20)[:5]) + half[::-1])
 
 def inbounds(pos):
     y, x = pos
     return 0 <= y < HEIGHT and 0 <= x < WIDTH
 
 
-class Directions(Enum):
-    UP = (-1, 0)
-    RIGHT = (0 , 1)
-    DOWN = (1, 0)
-    LEFT = (0, -1)
-
-
 class Snake(GraphicWidget):
-    def __init__(self, snake_color=AGREEN, apple_color=ARED, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.snake_color = snake_color
-        self.apple_color = apple_color
         self.reset()
         self._update_task = asyncio.create_task(self._update())
+        self._apple_task = asyncio.create_task(self._paint_apple())
 
     def on_size(self):
         super().on_size()
         self.reset()
 
     def reset(self):
-        # Empty positions allow us to easily find a place for an apple.
         self.texture[:] = self.default_color
 
+        # Keep track of empty positions to quickly find a place for an apple.
         self.empty_positions = list(product(range(HEIGHT), range(WIDTH)))
         self.empty_positions.remove(SNAKE_START)
 
         self.snake_is_moving = False
         self.snake = deque([SNAKE_START])
-        self.texture[SNAKE_START] = self.snake_color
+        self.texture[SNAKE_START] = RAINBOW_GRADIENT[0]
 
         self.apple = choice(self.empty_positions)
-        self.texture[self.apple] = self.apple_color
 
-        self.current_direction = Directions.RIGHT
+        self.current_direction = 0, 1
 
-    def move_snake(self):
-        dy, dx = self.current_direction.value
-        y, x = self.snake[0]
-        head = y + dy, x + dx
+    def _check_neck(self, dy, dx):
+        """
+        Don't allow the head to turn directly into the bit trailing it.
+        """
+        if len(self.snake) > 1:
+            y, x = self.snake[0]
+            if (dy + y, dx + x) == self.snake[1]:
+                return self.current_direction
 
-        if inbounds(head) and head not in self.snake:
-            self.empty_positions.remove(head)
-            self.snake.appendleft(head)
-            self.texture[head] = self.snake_color
-
-            if head == self.apple:
-                self.apple = choice(self.empty_positions)
-                self.texture[self.apple] = self.apple_color
-            else:
-                tail = self.snake.pop()
-                self.empty_positions.append(tail)
-                self.texture[tail] = self.default_color
-
-        else:
-            self.reset()
+        return dy, dx
 
     def on_press(self, key_press_event):
         if not self.snake_is_moving:
@@ -87,20 +69,44 @@ class Snake(GraphicWidget):
 
         match key_press_event.key:
             case "up":
-                self.current_direction = Directions.UP
+                self.current_direction = self._check_neck(-1, 0)
             case "left":
-                self.current_direction = Directions.LEFT
+                self.current_direction = self._check_neck(0, -1)
             case "right":
-                self.current_direction = Directions.RIGHT
+                self.current_direction = self._check_neck(0, 1)
             case "down":
-                self.current_direction = Directions.DOWN
+                self.current_direction = self._check_neck(1, 0)
             case " ":
                 self.snake_is_moving = False
+
+    async def _paint_apple(self):
+        while True:
+            self.texture[self.apple] = next(APPLE_GRADIENT)
+
+            await asyncio.sleep(TICK_DURATION / 2)
 
     async def _update(self):
         while True:
             if self.snake_is_moving:
-                self.move_snake()
+                dy, dx = self.current_direction
+                y, x = self.snake[0]
+                head = y + dy, x + dx
+
+                if inbounds(head) and head not in self.snake:
+                    self.empty_positions.remove(head)
+                    self.snake.appendleft(head)
+
+                    if head == self.apple:
+                        self.apple = choice(self.empty_positions)
+                    else:
+                        tail = self.snake.pop()
+                        self.empty_positions.append(tail)
+                        self.texture[tail] = self.default_color
+
+                    for point, color in zip(self.snake, cycle(RAINBOW_GRADIENT)):
+                        self.texture[point] = color
+                else:
+                    self.reset()
 
             await asyncio.sleep(TICK_DURATION)
 
@@ -113,9 +119,11 @@ class SnakeApp(App):
             anchor="center",
         )
 
-        background = Image(path=PATH_TO_LOGO_FULL, alpha=.33, **kwargs)
+        background = Animation(path=PATH_TO_PRIDE, alpha=.33, frame_durations=1, **kwargs)
+        background.play()
         snake = Snake(**kwargs)
 
         self.add_widgets(background, snake)
+
 
 SnakeApp().run()
