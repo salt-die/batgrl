16c8a238fcd338bb8fb2efc05f02a6b07b7eb404
salt-die <53280662+salt-die@users.noreply.github.com>
2022-08-31 12:11:17 -0500

    Themable documentation updated.

--- nurses_2/widgets/behaviors/themable.py
+++ nurses_2/widgets/behaviors/themable.py
@@ -9,19 +9,22 @@ from ...colors import DEFAULT_COLOR_THEME
 class Themable(ABC):
     """
     Themable widgets share a color theme. They must implement
-    :meth:`update_theme` which repaints the widget whenever the
-    color theme changes.
+    :meth:`update_theme` which paints the widget with current theme.
+
+    :meth:`update_theme` should be called at least once in the `__init__`.
+
+    Whenever the running app's theme is changed, all :class:`Themable` widgets
+    theme will be updated.
 
     Methods
     -------
     update_theme:
-        Repaint the widget with a new theme. This should be called at:
-        least once when a widget is initialized.
+        Paint the widget with current theme.
     """
     color_theme = DEFAULT_COLOR_THEME
 
     @abstractmethod
     def update_theme(self):
         """
-        Repaint the widget with a new theme.
+        Paint the widget with current theme.
         """
