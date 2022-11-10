ff51346c8d30f08d361019f767d9b1f37c432606
salt-die <53280662+salt-die@users.noreply.github.com>
2022-11-10 12:49:42 -0600

    Update example.

--- examples/basic/subscription.py
+++ examples/basic/subscription.py
@@ -1,19 +1,24 @@
 from nurses_2.app import App
+from nurses_2.colors import DEFAULT_COLOR_THEME
+from nurses_2.widgets.scroll_view import ScrollView
 from nurses_2.widgets.window import Window
 from nurses_2.widgets.text_widget import TextWidget
 
 
 class MyApp(App):
     async def on_start(self):
-        window = Window()
-
-        label = TextWidget(size=(2, 100))
+        window = Window(title="Move/Resize Me")
+        sv = ScrollView(allow_horizontal_scroll=False, show_horizontal_bar=False, show_vertical_bar=False)
+        label = TextWidget(size=(2, 100), default_color_pair=DEFAULT_COLOR_THEME.primary_color_pair)
 
         label.subscribe(window, "pos", lambda: label.add_text(f"{window.pos}"))
         label.subscribe(window, "size", lambda: label.add_text(f"{window.size}", row=1))
 
-        window.add_widget(label)
+        window.view = sv
+        sv.view = label
         self.add_widget(window)
+        window.size = 15, 30
+        window.pos = 10, 10
 
 
-MyApp(title="Widget Events Example").run()
+MyApp(title="Subscribe Example").run()
