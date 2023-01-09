f69f0776158b7699a5bed9574680cca2d17d903b
salt-die <53280662+salt-die@users.noreply.github.com>
2023-01-09 10:31:39 -0600

    Simplify.

--- examples/advanced/optical_illusion.py
+++ examples/advanced/optical_illusion.py
@@ -6,34 +6,23 @@ from nurses_2.app import App
 from nurses_2.widgets.graphic_widget import GraphicWidget, composite
 from nurses_2.colors import AColor
 
-H, W, LEN = 160, 160, 20
-
-BG_COLOR = AColor.from_hex("6d42b2")
-FG_COLOR = AColor.from_hex("9c42b2")
-
+LEN = 20
+COLORS = AColor.from_hex("6d42b2"), AColor.from_hex("9c42b2")
 DARK = np.zeros((6, 6, 4), np.uint8)
 DARK[:, 2:4] = DARK[2:4, :] = AColor.from_hex("200d4c")
-
 LIGHT = np.zeros_like(DARK)
 LIGHT[:, 2:4] = LIGHT[2:4, :] = AColor.from_hex("b24290")
-
-PATTERN = [LIGHT, LIGHT, DARK, LIGHT, DARK, DARK, LIGHT, DARK]
+PATTERN = LIGHT, LIGHT, DARK, LIGHT, DARK, DARK, LIGHT, DARK
+NCHECKS = len(PATTERN)
 
 class IllusionApp(App):
     async def on_start(self):
-        illusion = GraphicWidget(size=(H // 2, W), default_color=BG_COLOR)
+        illusion = GraphicWidget(size=(NCHECKS * LEN // 2, NCHECKS * LEN))
 
-        rows = H // LEN
-        cols = W // LEN
-        for y in range(rows):
-            for x in range(cols):
-                if (y + x) % 2 == 0:
-                    illusion.texture[y * LEN: (y + 1) * LEN, x * LEN: (x + 1) * LEN] = FG_COLOR
-
-        for y in range(rows + 1):
-            for x in range(cols + 1):
-                cross = PATTERN[(x - y) % len(PATTERN)]
-                composite(cross, illusion.texture, (y * LEN - 3, x * LEN - 3))
+        for y in range(NCHECKS + 1):
+            for x in range(NCHECKS + 1):
+                illusion.texture[y * LEN: (y + 1) * LEN, x * LEN: (x + 1) * LEN] = COLORS[(x + y) % 2]
+                composite(PATTERN[(x - y) % NCHECKS], illusion.texture, (y * LEN - 3, x * LEN - 3))
 
         self.add_widget(illusion)
         while True:
