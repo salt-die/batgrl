a62ef285b829beb53e7350dfab1f34ff64c70801
salt-die <53280662+salt-die@users.noreply.github.com>
2023-04-13 17:50:25 -0500

    Spinning Cube example added.

---
+++ examples/advanced/spinning_cube.py
@@ -0,0 +1,119 @@
+import asyncio
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from nurses_2.app import App
+from nurses_2.colors import rainbow_gradient
+from nurses_2.widgets.graphic_widget import GraphicWidget
+from nurses_2.widgets.image import Image
+
+ASSETS = Path(__file__).parent.parent / "assets"
+BACKGROUND = ASSETS / "loudypixelsky.png"
+RAINBOW = rainbow_gradient(12)
+POINTS = np.array([
+    [-1, -1, -1],
+    [-1, -1,  1],
+    [-1,  1, -1],
+    [-1,  1,  1],
+    [ 1, -1, -1],
+    [ 1, -1,  1],
+    [ 1,  1, -1],
+    [ 1,  1,  1],
+])
+LINES = [
+    [0, 1], [0, 2], [0, 4], [1, 3], [1, 5], [2, 3],
+    [2, 6], [3, 7], [4, 5], [4, 6], [5, 7], [6, 7],
+]
+RADIUS = 3**.5  # The cube of points is inscribed in a circle with this radius
+
+def rotate_x(theta):
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+
+    return np.array([
+        [1,    0,   0],
+        [0,  cos, sin],
+        [0, -sin, cos],
+    ])
+
+def rotate_y(theta):
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+
+    return np.array([
+        [cos, 0, -sin],
+        [  0, 1,    0],
+        [sin, 0,  cos],
+    ])
+
+def rotate_z(theta):
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+
+    return np.array([
+        [ cos, sin, 0],
+        [-sin, cos, 0],
+        [   0,   0, 1],
+    ])
+
+
+class SpinningCube(GraphicWidget):
+    def on_add(self):
+        super().on_add()
+        self._spin_task = asyncio.create_task(self.spin_forever())
+
+    def on_remove(self):
+        self._spin_task.cancel()
+        super().on_remove()
+
+    async def spin_forever(self):
+        x_angle = y_angle = z_angle = 0
+
+        while True:
+            self.texture[:] = self.default_color
+
+            h, w = self.size
+            scale = w // 4, h // 2
+            offset = w // 2, h
+
+            points = POINTS @ rotate_x(x_angle) @ rotate_y(y_angle) @ rotate_z(z_angle)
+            points_2D = (points[:, 1::-1] * scale + offset).astype(int)
+            depths = points[:, 2]
+
+            lines = []
+            for (a, b), (r, g, bl) in zip(LINES, RAINBOW):
+                # Darkness of the line will depend on its depth.
+                average_depth = (depths[a] + depths[b]) / 2
+                normalized_depth = (average_depth + RADIUS) / (2 * RADIUS)
+                min_darkness = .15
+                darkness = min_darkness + normalized_depth * (1 - min_darkness)
+                new_color = int(darkness * r), int(darkness * g), int(darkness * bl), 255
+                lines.append((
+                    points_2D[a],
+                    points_2D[b],
+                    new_color,
+                    darkness,
+                ))
+            lines.sort(key=lambda p: p[3])
+
+            for a, b, color, _ in lines:
+                cv2.line(self.texture, a, b, color, 2)
+
+            x_angle += .001333
+            y_angle += .002
+            z_angle += .002666
+
+            await asyncio.sleep(0)
+
+
+class SpinApp(App):
+    async def on_start(self):
+        self.add_widgets(
+            Image(path=BACKGROUND, size_hint=(1.0, 1.0)),
+            SpinningCube(size_hint=(1.0, 1.0)),
+        )
+
+
+SpinApp(title="Spinning Cube").run()
