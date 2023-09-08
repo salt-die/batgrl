fd4c71ad371048ce5bfb9b372544c1706a30ffbd
salt-die <53280662+salt-die@users.noreply.github.com>
2023-09-08 16:03:23 -0500

    Rigid body phyics example added.

---
+++ examples/advanced/rigid_body_physics.py
@@ -0,0 +1,189 @@
+"""
+A rigid body physics simulation in the terminal!
+
+Requires `pymunk`
+"""
+import asyncio
+from enum import Enum
+from pathlib import Path
+from math import degrees, ceil
+
+import cv2
+
+import numpy as np
+
+from nurses_2.app import App
+from nurses_2.colors import AColor, AWHITE
+from nurses_2.widgets.graphic_widget import GraphicWidget, read_texture, composite, resize_texture
+from nurses_2.widgets.image import Image
+
+import pymunk
+from pymunk.vec2d import Vec2d
+
+
+class RenderMode(str, Enum):
+    OUTLINE = "outline"
+    FILL = "fill"
+    SPRITE = "sprite"
+
+
+BOX_SIZE = W, H = Vec2d(9, 9)
+BOX_MASS = .1
+BALL_MASS = 100
+BALL_RADIUS = 6
+GROUND_LEVEL = 0
+
+BOX_CENTER = HW, HH = W // 2, H // 2
+BOX_POINTS = np.array([[0, 0], [0, W], [0, H], [W, H]]).reshape(-1, 1, 2)
+BALL_CENTER = BALL_RADIUS, BALL_RADIUS
+BALL_SIZE = BALL_RADIUS * 2, BALL_RADIUS * 2
+
+ASSETS = Path(__file__).parent.parent / "assets"
+PATH_TO_BACKGROUND = ASSETS / "background.png"
+PATH_TO_CRATE = ASSETS / "crate.png"
+PATH_TO_BALL = ASSETS / "soccer_ball.png"
+CRATE = resize_texture(read_texture(PATH_TO_CRATE), BOX_SIZE, "nearest")
+BALL = resize_texture(read_texture(PATH_TO_BALL), BALL_SIZE, "nearest")
+
+
+class SpaceRenderer(GraphicWidget):
+    def __init__(
+        self,
+        space: pymunk.Space,
+        dt: float=.01,
+        render_mode: RenderMode=RenderMode.SPRITE,
+        shape_color: AColor=AWHITE,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.space = space
+        self.dt = dt
+        self.render_mode = RenderMode(render_mode)
+        self.shape_color = shape_color
+
+    def stop_simulation(self):
+        if task := getattr(self, "_simulation_task", False):
+            task.cancel()
+
+    def on_remove(self):
+        self.stop_simulation()
+
+    def run_simulation(self):
+        self.stop_simulation()
+        self._simulation_task = asyncio.create_task(self._run_simulation())
+
+    async def _run_simulation(self):
+        while True:
+            self.space.step(self.dt)
+            self._draw_space()
+            await asyncio.sleep(0)
+
+    def _to_texture_coords(self, point: Vec2d) -> tuple[int, int]:
+        x, y = point
+        return round(x), 2 * self.height - round(y) - 1
+
+    def _draw_space(self):
+        self.texture[:] = self.default_color
+        to_tex_coords = self._to_texture_coords
+
+        for shape in self.space.shapes:
+            if isinstance(shape, pymunk.shapes.Segment):
+                if self.render_mode is not RenderMode.SPRITE:
+                    a = shape.a.rotated(shape.body.angle) + shape.body.position
+                    b = shape.b.rotated(shape.body.angle) + shape.body.position
+                    cv2.line(self.texture, to_tex_coords(a), to_tex_coords(b), self.shape_color)
+            elif isinstance(shape, pymunk.shapes.Poly):
+                if self.render_mode is RenderMode.SPRITE:
+                    angle = degrees(shape.body.angle)
+                    rot = cv2.getRotationMatrix2D(BOX_CENTER, angle, 1)
+                    box = cv2.boxPoints((BOX_CENTER, BOX_SIZE, angle))
+                    min_x = box[:, 0].min()
+                    min_y = box[:, 1].min()
+                    max_x = box[:, 0].max()
+                    max_y = box[:, 1].max()
+                    box_w = ceil(max_x - min_x)
+                    box_h = ceil(max_y - min_y)
+                    box_hw = box_w // 2
+                    box_hh = box_h // 2
+                    rot[0, 2] += box_hw - HW
+                    rot[1, 2] += box_hh - HH
+                    src = cv2.warpAffine(CRATE, rot, (box_w, box_h))
+                    x, y = to_tex_coords(shape.body.position + (-box_hw, box_hh))
+                    composite(src, self.texture, (y, x))
+                else:
+                    vertices = np.array([
+                        to_tex_coords(vertex.rotated(shape.body.angle) + shape.body.position)
+                        for vertex in shape.get_vertices()
+                    ])
+                    if self.render_mode is RenderMode.FILL:
+                        cv2.fillPoly(self.texture, [vertices], color=self.shape_color)
+                    else:
+                        cv2.polylines(self.texture, [vertices], isClosed=True, color=self.shape_color)
+            elif isinstance(shape, pymunk.shapes.Circle):
+                if self.render_mode is RenderMode.SPRITE:
+                    angle == degrees(shape.body.angle)
+                    rot = cv2.getRotationMatrix2D(BALL_CENTER, angle, 1)
+                    src = cv2.warpAffine(BALL, rot, BALL_SIZE)
+                    x, y = to_tex_coords(shape.body.position + (-BALL_RADIUS, BALL_RADIUS))
+                    composite(src, self.texture, (y, x))
+                else:
+                    pos = shape.body.position
+                    center = to_tex_coords(pos)
+                    circle_edge = pos + Vec2d(shape.radius, 0).rotated(shape.body.angle)
+                    if self.render_mode is RenderMode.FILL:
+                        cv2.circle(self.texture, center, round(shape.radius), self.shape_color, -1)
+                    else:
+                        cv2.circle(self.texture, center, round(shape.radius), self.shape_color)
+                        cv2.line(self.texture, center, to_tex_coords(circle_edge), self.shape_color)
+
+
+class PhysicsApp(App):
+    async def on_start(self):
+        space = pymunk.Space()
+        space.gravity = 0, -20
+        space.damping = 0.9
+
+        # Boxes
+        for i in range(3):
+            for j in range(3 - i):
+                body = pymunk.Body(BOX_MASS, pymunk.moment_for_box(BOX_MASS, BOX_SIZE))
+                body.position = (
+                    1.3 * BOX_SIZE.x * i + GROUND_LEVEL + 5,
+                    1.3 * BOX_SIZE.y * j + 10,
+                )
+                box = pymunk.Poly.create_box(body, size=BOX_SIZE)
+                box.elasticity = 0.3
+                box.friction = 0.8
+                space.add(body, box)
+
+        # Ball
+        body = pymunk.Body(BALL_MASS, pymunk.moment_for_circle(BALL_MASS, 0, BALL_RADIUS))
+        body.position = 100, 30
+        body.velocity = -75, 5
+        body.angular_velocity = -5
+        ball = pymunk.Circle(body, BALL_RADIUS)
+        ball.elasticity = 1
+        ball.friction = 0.8
+        space.add(body, ball)
+
+        # Ground
+        segment = pymunk.Segment(space.static_body, (0, GROUND_LEVEL), (540, GROUND_LEVEL), 1)
+        segment.elasticity = 1
+        segment.friction = 1.0
+        space.add(segment)
+
+        # Vertical Wall
+        segment = pymunk.Segment(space.static_body, (0, GROUND_LEVEL), (0, 200), 1)
+        segment.elasticity = 1
+        segment.friction = 1.0
+        space.add(segment)
+
+        space_renderer = SpaceRenderer(space, render_mode="sprite", dt=.03, size_hint=(1.0, 1.0))
+        space_renderer.run_simulation()
+
+        background = Image(path=PATH_TO_BACKGROUND, size_hint=(1.0, 1.0))
+
+        self.add_widgets(background, space_renderer)
+
+
+PhysicsApp(title="Rigid Body Physics Simulation").run()
