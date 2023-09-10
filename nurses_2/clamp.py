12ce1e1acb6865386bc23f9c4dc4fc780cca3861
salt-die <53280662+salt-die@users.noreply.github.com>
2023-09-10 11:40:49 -0500

    Update hinting and docs for clamp.

--- nurses_2/clamp.py
+++ nurses_2/clamp.py
@@ -1,12 +1,27 @@
 """
-Clamp a value between min and max.
+Clamp a value between a min and a max.
 """
 from numbers import Real
 
 
-def clamp(value, min: Real | None, max: Real | None):
+def clamp(value: Real, min: Real | None, max: Real | None) -> Real:
     """
-    Clamp a value between min and max.
+    If `value` is less than `min`, returns `min`; otherwise if `max` is less than
+    `value`, returns `max`; otherwise returns `value`.
+
+    Parameters
+    ----------
+    value : Real
+        Value to clamp.
+    min : Real | None
+        Minimum of clamped value.
+    max : Real | None
+        Maximum of clamped value.
+
+    Returns
+    -------
+    Real
+        A value between `min` and `max`, inclusive.
     """
     if min is not None and value < min:
         return min
