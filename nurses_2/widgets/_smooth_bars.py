78692fc33ab595e03b44ca71329254246df269d9
salt-die <53280662+salt-die@users.noreply.github.com>
2023-09-18 18:39:02 -0500

    Added `create_vertical_bar_offset_half` helper function.

--- nurses_2/widgets/_smooth_bars.py
+++ nurses_2/widgets/_smooth_bars.py
@@ -4,6 +4,7 @@ Characters and functions to create smooth bars.
 FULL_BLOCK = "█"
 VERTICAL_BLOCKS = " ▁▂▃▄▅▆▇█"
 HORIZONTAL_BLOCKS = " ▏▎▍▌▋▊▉█"
+UPPER_HALF_BLOCK = "▀"
 
 
 def create_vertical_bar(max_height: int, proportion: float) -> tuple[int, str, str]:
@@ -28,3 +29,22 @@ def create_horizontal_bar(max_width: int, proportion: float) -> tuple[int, str, 
         return int(fill) - 1, FULL_BLOCK, FULL_BLOCK
     index_partial = round(partial * (len(HORIZONTAL_BLOCKS) - 1))
     return int(fill), FULL_BLOCK, HORIZONTAL_BLOCKS[index_partial]
+
+
+def create_vertical_bar_offset_half(
+    max_height: int, proportion: float
+) -> tuple[str, int, str, str]:
+    """
+    Returns half block character for bottom of the bar, number of full blocks and the
+    full block character for the middle of the bar, and the partial block character
+    needed to create a vertical bar that fills `max_height` by `proportion`.
+    """
+    fill, partial = divmod(proportion * max_height, 1)
+    partial += 0.5
+    if partial > 1:
+        partial -= 1
+    else:
+        fill -= 1
+
+    index_partial = round(partial * (len(VERTICAL_BLOCKS) - 1))
+    return UPPER_HALF_BLOCK, int(fill), FULL_BLOCK, VERTICAL_BLOCKS[index_partial]
