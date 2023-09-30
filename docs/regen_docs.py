b661956d42aadad23e9339381382b70aaa40ee20
salt-die <53280662+salt-die@users.noreply.github.com>
2023-09-30 03:26:43 -0500

    Remove html from main.

--- docs/regen_docs.py
+++ docs/regen_docs.py
@@ -3,29 +3,47 @@ Rebuild nurses_2 documentation.
 """
 from pathlib import Path
 
-DOC_SRC = Path(__file__).absolute().parent
-
+DOCS = Path(__file__).absolute().parent
 SPHINX_BUILD = "sphinx-build"
-SOURCE_DIR = "."
 BUILD_DIR = "_build"
-SPHINX_COMMAND = [SPHINX_BUILD, "-M", "html", SOURCE_DIR, BUILD_DIR]
-
-DIRECTORIES_TO_REMOVE = (
-    DOC_SRC / BUILD_DIR,
-    DOC_SRC / "reference" / "generated",
+DIRECTORIES_TO_CLEAN = (
+    DOCS / BUILD_DIR,
+    DOCS / "reference" / "generated",
 )
+
+
+def recursive_remove_dir(path: Path):
+    for child in path.iterdir():
+        if child.is_file():
+            child.unlink()
+        else:
+            recursive_remove_dir(child)
+    path.rmdir()
+
 
 if __name__ == "__main__":
     import os
-    import shutil
     import subprocess
 
-    os.chdir(DOC_SRC)
-
-    for directory in DIRECTORIES_TO_REMOVE:
+    for directory in DIRECTORIES_TO_CLEAN:
         if directory.exists():
-            shutil.rmtree(directory)
+            for path in directory.iterdir():
+                if (
+                    path.is_file()
+                    and path.suffix != ".git"
+                    and path.suffix != ".pre-commit-config.yaml"
+                ):
+                    path.unlink()
+                elif path.is_dir():
+                    recursive_remove_dir(path)
 
-    subprocess.run(SPHINX_COMMAND)
+    if not (DOCS / BUILD_DIR).exists():
+        (DOCS / BUILD_DIR).mkdir()
 
-    (DOC_SRC / BUILD_DIR / "html" / ".nojekyll").touch()
+    os.chdir(DOCS / BUILD_DIR)
+    subprocess.run(["git", "worktree", "add", "-f", "html", "gh-pages"])
+
+    os.chdir(DOCS)
+    subprocess.run([SPHINX_BUILD, "-M", "html", ".", BUILD_DIR])
+
+    (DOCS / BUILD_DIR / "html" / ".nojekyll").touch()
