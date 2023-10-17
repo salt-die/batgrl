b27449c9949aa17f8c325dacd8ba6d7f9bf69e0c
salt-die <53280662+salt-die@users.noreply.github.com>
2023-10-17 18:29:19 -0500

    Fix syntax highlighting for multiline values.

--- examples/advanced/syntax_highlighting.py
+++ examples/advanced/syntax_highlighting.py
@@ -5,14 +5,18 @@ Requires `pygments>=2.14`.
 
 `add_syntax_highlighting` may be included in `batgrl` in the future.
 """
-from batgrl.app import App
-from batgrl.colors import Color
-from batgrl.gadgets.text import Text
+from pathlib import Path
+
 from pygments.lexer import Lexer
 from pygments.lexers import get_lexer_by_name
 from pygments.style import Style
 from pygments.styles import get_style_by_name
 from wcwidth import wcswidth
+
+from batgrl.app import App
+from batgrl.colors import Color
+from batgrl.gadgets.scroll_view import ScrollView
+from batgrl.gadgets.text import Text
 
 
 def add_syntax_highlighting(text_gadget: Text, lexer: Lexer, style: Style):
@@ -38,45 +42,41 @@ def add_syntax_highlighting(text_gadget: Text, lexer: Lexer, style: Style):
 
     text = "\n".join("".join(line).rstrip() for line in canvas["char"])
     for ttype, value in lexer.get_tokens(text):
-        if value == "\n":
-            y += 1
-            x = 0
-            continue
-
+        lines = value.split("\n")
         token_style = style.style_for_token(ttype)
-        end = x + wcswidth(value)
-        if token_style["color"]:
-            colors[y, x:end, :3] = Color.from_hex(token_style["color"])
-        if token_style["bgcolor"]:
-            colors[y, x:end, 3:] = Color.from_hex(token_style["bgcolor"])
-        canvas[y, x:end]["bold"] = token_style["bold"]
-        canvas[y, x:end]["italic"] = token_style["italic"]
-        canvas[y, x:end]["underline"] = token_style["underline"]
-        x = end
+
+        for i, line in enumerate(lines):
+            if i > 0:
+                y += 1
+                x = 0
+            end = x + wcswidth(line)
+            if token_style["color"]:
+                colors[y, x:end, :3] = Color.from_hex(token_style["color"])
+            if token_style["bgcolor"]:
+                colors[y, x:end, 3:] = Color.from_hex(token_style["bgcolor"])
+            canvas[y, x:end]["bold"] = token_style["bold"]
+            canvas[y, x:end]["italic"] = token_style["italic"]
+            canvas[y, x:end]["underline"] = token_style["underline"]
+            x = end
 
 
 PYTHON_LEXER = get_lexer_by_name("Python")
 STYLE = get_style_by_name("github-dark")
-CODE = """\
-class SyntaxApp(App):
-    async def on_start(self):
-        text_gadget = Text()
-        text_gadget.set_text(CODE)
-        self.add_gadget(text_gadget)
-        add_syntax_highlighting(text_gadget, PYTHON_LEXER, STYLE)
-
-
-if __name__ == "__main__":
-    SyntaxApp().run()
-"""
 
 
 class SyntaxApp(App):
     async def on_start(self):
-        text_gadget = Text()
-        text_gadget.set_text(CODE)
-        self.add_gadget(text_gadget)
-        add_syntax_highlighting(text_gadget, PYTHON_LEXER, STYLE)
+        code = Path(__file__).read_text()
+        text = Text()
+        text.set_text(code)
+        add_syntax_highlighting(text, PYTHON_LEXER, STYLE)
+        sv = ScrollView(
+            size=(1, text.width + 2),
+            size_hint={"height_hint": 1.0},
+            show_horizontal_bar=False,
+        )
+        sv.view = text
+        self.add_gadget(sv)
 
 
 if __name__ == "__main__":
