3500b20f58f6d76d4d257e193181723f3249880f
salt-die <53280662+salt-die@users.noreply.github.com>
2023-11-19 03:18:06 -0600

    Change widget to gadget.

--- examples/basic/scroll_view.py
+++ examples/basic/scroll_view.py
@@ -7,24 +7,24 @@ from batgrl.gadgets.scroll_view import ScrollView
 from batgrl.gadgets.text import Size, Text
 
 N = 20  # Number of coordinate pairs on each line.
-BIG_WIDGET_SIZE = Size(50, 8 * N + N - 1)
+BIG_GADGET_SIZE = Size(50, 8 * N + N - 1)
 
 WHITE_ON_RED = ColorPair.from_colors(WHITE, RED)
 WHITE_ON_GREEN = ColorPair.from_colors(WHITE, GREEN)
 WHITE_ON_BLUE = ColorPair.from_colors(WHITE, BLUE)
 
-LEFT_GRADIENT = gradient(WHITE_ON_RED, WHITE_ON_GREEN, BIG_WIDGET_SIZE.rows)
-RIGHT_GRADIENT = gradient(WHITE_ON_GREEN, WHITE_ON_BLUE, BIG_WIDGET_SIZE.rows)
+LEFT_GRADIENT = gradient(WHITE_ON_RED, WHITE_ON_GREEN, BIG_GADGET_SIZE.rows)
+RIGHT_GRADIENT = gradient(WHITE_ON_GREEN, WHITE_ON_BLUE, BIG_GADGET_SIZE.rows)
 
 
 class ScrollViewApp(App):
     async def on_start(self):
-        big_gadget = Text(size=BIG_WIDGET_SIZE)
+        big_gadget = Text(size=BIG_GADGET_SIZE)
 
-        for y in range(BIG_WIDGET_SIZE.rows):
+        for y in range(BIG_GADGET_SIZE.rows):
             big_gadget.add_str(" ".join(f"({y:<2}, {x:<2})" for x in range(N)), (y, 0))
             big_gadget.colors[y] = gradient(
-                LEFT_GRADIENT[y], RIGHT_GRADIENT[y], BIG_WIDGET_SIZE.columns
+                LEFT_GRADIENT[y], RIGHT_GRADIENT[y], BIG_GADGET_SIZE.columns
             )
 
         scroll_view = ScrollView(size=(10, 30), pos_hint={"y_hint": 0.5, "x_hint": 0.5})
