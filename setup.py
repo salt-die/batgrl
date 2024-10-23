1342f66636945725b902cf2563ef552bad1bcbcb
salt-die <salt-die@protonmail.com>
2024-10-22 22:52:31 -0500

    Add `_rendering.pyx`.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
             "src/batgrl/geometry/regions.pyx",
             "src/batgrl/colors/quantization.pyx",
             "src/batgrl/_sixel.pyx",
+            "src/batgrl/_rendering.pyx",
         ]
     ),
     include_dirs=[np.get_include()],
