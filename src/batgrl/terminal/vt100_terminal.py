5a60f0d8a1d09d4efe230da04805de6eb94eb330
bp <badpazzword@gmail.com>
2025-01-26 00:29:48 +0100

    vt100_terminal.py: fix (?) mouse support on Linux
    This patch restores batgrl's ability to work with:
    
    - xterm
    - konsole
    - kitty
    - ghostty
    
    I wish I could explain why changing the order of these commands is
    necessary! This patch was authored through trial and error. However, it
    just so happens to bring back enable_mouse_support to the contents it
    had in e726ea8de003fb3f0fd9c59dc2bc2c8929b051ea. It appears that this
    might be a bug that was introduced in the refactoring of commit
    2f594edae4388f1502b78789aa31957abd264dfe ("v0.35.0") and made
    enable_mouse_support and disable_mouse_support stop looking alike.

--- src/batgrl/terminal/vt100_terminal.py
+++ src/batgrl/terminal/vt100_terminal.py
@@ -401,8 +401,8 @@ class Vt100Terminal(ABC):
         self._out_buffer.append(
             "\x1b[?1000h"  # SET_VT200_MOUSE
             "\x1b[?1003h"  # SET_ANY_EVENT_MOUSE
-            "\x1b[?1006h"  # SET_SGR_EXT_MODE_MOUSE
             "\x1b[?1015h"  # SET_URXVT_EXT_MODE_MOUSE
+            "\x1b[?1006h"  # SET_SGR_EXT_MODE_MOUSE
         )
 
     def disable_mouse_support(self) -> None:
