5fed644a16a0d5e728a9c0785ceb8f169614876a
salt-die <salt-die@protonmail.com>
2025-05-28 23:33:27 -0500

    Add some doodads to example. [no ci]

--- examples/advanced/navier_stokes.py
+++ examples/advanced/navier_stokes.py
@@ -7,11 +7,15 @@ texture's size plus a 2-width border and copying data into that border.
 """
 
 import asyncio
+from typing import Literal
 
 import numpy as np
-from batgrl.app import run_gadget_as_app
-from batgrl.colors import AColor
+from batgrl.app import App
+from batgrl.colors import Color
 from batgrl.gadgets.graphics import Graphics, scale_geometry
+from batgrl.gadgets.slider import Slider
+from batgrl.gadgets.text import Text
+from batgrl.gadgets.toggle_button import ToggleButton
 from cv2 import filter2D
 
 # Kernels
@@ -36,8 +40,8 @@ POISSON = np.array(
         [0, 0.25, 0],
     ]
 )
-
-WATER_COLOR = AColor.from_hex("1259FF")
+WATER_COLOR = Color.from_hex("1259FF")
+BINARY = np.array([Color.from_hex("0606fa"), Color.from_hex("0f080e")])
 
 
 def wrap_border(array):
@@ -52,6 +56,10 @@ def sigmoid(array):
 
 
 class Fluid(Graphics):
+    mode: Literal["smooth", "binary"] = "smooth"
+    visual: Literal["momentum", "pressure"] = "pressure"
+    damping: float = 0.99
+
     def on_add(self):
         super().on_add()
         self.on_size()
@@ -98,18 +106,85 @@ class Fluid(Graphics):
             delta = filter2D(self.momentum, -1, POISSON)
 
             self.pressure = filter2D(pressure, -1, POISSON) + 0.5 * (delta - delta**2)
-            self.pressure *= 0.99
+            self.pressure *= self.damping
 
-            # Note the alpha channel is affected by `pressure` as well.
-            self.texture[:] = (
-                sigmoid(self.pressure[2:-2, 2:-2, None]) * WATER_COLOR
-            ).astype(np.uint8)
+            if self.visual == "momentum":
+                arr = self.momentum
+            else:
+                arr = self.pressure
 
+            if self.mode == "binary":
+                indices = (sigmoid(arr[2:-2, 2:-2]) * 2).astype(int)
+                self.texture[..., :3] = BINARY[indices]
+            else:
+                self.texture[..., :3] = (
+                    sigmoid(arr[2:-2, 2:-2, None]) * WATER_COLOR
+                ).astype(np.uint8)
+            self.texture[..., 3] = 255
             await asyncio.sleep(0)
 
 
+class NavierStokesApp(App):
+    async def on_start(self):
+        fluid = Fluid(size_hint={"height_hint": 1.0, "width_hint": 1.0})
+
+        def on_smooth(toggle_state):
+            if toggle_state == "on":
+                fluid.mode = "smooth"
+
+        def on_binary(toggle_state):
+            if toggle_state == "on":
+                fluid.mode = "binary"
+
+        smooth_toggle = ToggleButton(
+            label="smooth", group=0, callback=on_smooth, size=(1, 9)
+        )
+        binary_toggle = ToggleButton(
+            label="binary", group=0, callback=on_binary, pos=(1, 0), size=(1, 9)
+        )
+
+        def on_momentum(toggle_state):
+            if toggle_state == "on":
+                fluid.visual = "momentum"
+
+        def on_pressure(toggle_state):
+            if toggle_state == "on":
+                fluid.visual = "pressure"
+
+        momentum_toggle = ToggleButton(
+            label="momentum", group=1, callback=on_momentum, size=(1, 11), pos=(0, 10)
+        )
+        pressure_toggle = ToggleButton(
+            label="pressure", group=1, callback=on_pressure, size=(1, 11), pos=(1, 10)
+        )
+
+        damp_label = Text(size=(1, 20), pos=(2, 0), is_transparent=True)
+
+        def on_damping(value):
+            fluid.damping = 1 - value
+            damp_label.add_str(f"Damping: {value:.2f}".center(20))
+
+        damping = Slider(
+            min=0.0,
+            max=0.2,
+            start_value=0.01,
+            callback=on_damping,
+            size=(1, 20),
+            pos=(3, 0),
+            is_transparent=True,
+            alpha=0.0,
+        )
+
+        self.add_gadgets(
+            fluid,
+            smooth_toggle,
+            binary_toggle,
+            momentum_toggle,
+            pressure_toggle,
+            damp_label,
+            damping,
+        )
+
+
 if __name__ == "__main__":
-    run_gadget_as_app(
-        Fluid(size_hint={"height_hint": 1.0, "width_hint": 1.0}),
-        title="Fluid Simulation",
-    )
+    NavierStokesApp(title="Fluid Simulation").run()
