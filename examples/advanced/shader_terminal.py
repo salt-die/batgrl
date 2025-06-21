4b0fbc51df6c86e01abadb6e36b496d0cc4be90e
salt-die <salt-die@protonmail.com>
2025-06-20 20:45:24 -0500

    Change to shorter shader with mouse interaction [no ci]

--- examples/advanced/shader_terminal.py
+++ examples/advanced/shader_terminal.py
@@ -4,7 +4,7 @@
 #   "moderngl",
 # ]
 # ///
-"""Shadertoy example: https://www.shadertoy.com/view/WfcGWj using moderngl."""
+"""Shadertoy https://www.shadertoy.com/view/3XXSDB by XorDev using moderngl."""
 
 import asyncio
 import time
@@ -12,7 +12,7 @@ import time
 import moderngl
 import numpy as np
 from batgrl.app import App
-from batgrl.gadgets.graphics import Graphics
+from batgrl.gadgets.graphics import Graphics, scale_geometry
 
 VERTEX_SHADER = """
     out vec2 position;
@@ -25,82 +25,79 @@ VERTEX_SHADER = """
 FRAGMENT_SHADER = """
 in vec2 position;
 
-uniform vec2 iResolution;
 uniform float iTime;
+uniform vec2 iResolution;
+uniform vec4 iMouse;
 
-float g(vec4 p,float s) {
-  p.x = -abs(p.x);
-  return abs(dot(sin(p *= s),cos(p.zxwy)) - 1.) / s;
-}
-
-vec4 mainImage(in vec2 uv) {
-  float i, d, z, s, T = iTime * 5;
-  vec4 o, q, p, U = vec4(2, 1, 0, 3);
-  for (
-    vec2 r = iResolution.xy;
-    ++i < 79.;
-    z += d + 5E-4
-    , q = vec4(normalize(vec3(uv - .5 * r, r.y)) * z, .2)
-    , q.z += T / 3E1
-    , s = q.y + .1
-    , q.y = abs(s)
-    , p = q
-    , p.y -= .11
-    , p.xy *= mat2(cos(11. * U.zywz - 2. * p.z))
-    , p.y -= .2
-    , d = abs(g(p, 8.) - g(p, 24.)) / 4.
-    , p = 1. + cos(.7 * U + 5. * q.z)
-  )
-    o += (s > 0. ? 1. : .1) * p.w * p / max(s > 0. ? d : d*d*d, 5E-4);
-
-  o += (1.4 + sin(T) * sin(1.7 * T) * sin(2.3 * T)) * 1E3 * U / length(q.xy);
-
-  return tanh(o / 1E5);
+vec4 mainImage(in vec2 I) {
+    float t = iTime, i, z, d;
+    vec4 O;
+    for(O *= i; i++ < 1e2; O += (sin(z + vec4(2, 3, 4, 0)) + 1.1) / d)
+    {
+        vec3 p = z * normalize(vec3(I + I, 0) - iResolution.xyy);
+        p.z += iMouse.x / iResolution.x * 20.;
+        p.xz *= mat2(cos(p.y * .5 + vec4(0, 33, 11, 0)));
+        for(d = 1.; d < 9.; d /= .8) p += cos((p.yzx - t * vec3(3, 1, 0)) * d) / d;
+        z += d = (.1 + abs(length(p.xz) - .5)) / 2e1;
+    }
+    return tanh(O / 4e3);
 }
 
 void main() {
     gl_FragColor = mainImage(position * iResolution);
 }
 """
-context = moderngl.create_context(standalone=True)
-program = context.program(vertex_shader=VERTEX_SHADER, fragment_shader=FRAGMENT_SHADER)
-vao = context.vertex_array(program, [])
-vao.vertices = 3
+
+
+class GlGraphics(Graphics):
+    def __init__(self, *args, **kwargs):
+        self.context = moderngl.create_context(standalone=True)
+        self.program = self.context.program(
+            vertex_shader=VERTEX_SHADER, fragment_shader=FRAGMENT_SHADER
+        )
+        super().__init__(*args, **kwargs)
+        self.start = time.perf_counter()
+        self.vao = self.context.vertex_array(self.program, [])
+        self.vao.vertices = 3
+        self.on_size()  # Create fbo
+
+    def on_size(self):
+        super().on_size()
+        h, w = self.texture.shape[:2]
+        self.program["iResolution"] = w, h
+        self.fbo = self.context.simple_framebuffer((w, h), components=4, dtype="f4")
+        self.fbo.use()
+
+    def update(self):
+        self.program["iTime"].value = time.perf_counter() - self.start
+        self.vao.render()
+        fbo_read = self.fbo.read(components=4, dtype="f4")
+        h, w = self.texture.shape[:2]
+        self.texture[::-1] = (
+            (np.frombuffer(fbo_read, dtype="f4") * 255)
+            .astype(np.uint8)
+            .reshape(h, w, 4)
+        )
+
+    def on_mouse(self, mouse_event):
+        y, x = scale_geometry(self.blitter, mouse_event.pos)
+        y = self.texture.shape[0] - y
+        w = mouse_event.button != "no_button"
+        self.program["iMouse"] = x, y, w, w
+        return w
 
 
 class ModernGlApp(App):
     async def on_start(self):
-        graphics = Graphics(
+        graphics = GlGraphics(
             size_hint={"height_hint": 1.0, "width_hint": 1.0},
-            # alpha channel will be all 0, so set is_transparent to False to ignore
-            is_transparent=False,
             blitter="sextant",
+            is_transparent=False,
         )
         self.add_gadget(graphics)
 
-        h = w = fbo = None
-
-        def on_size():
-            nonlocal h, w, fbo
-            h, w = graphics.texture.shape[:2]
-            program["iResolution"] = w, h
-            fbo = context.simple_framebuffer((w, h), components=4, dtype="f4")
-            fbo.use()
-
-        on_size()
-        graphics.bind("size", on_size)
-
-        start = time.perf_counter()
         while True:
-            program["iTime"].value = time.perf_counter() - start
-            vao.render()
-
-            fbo_read = fbo.read(components=4, dtype="f4")
-            graphics.texture[::-1] = (
-                (np.frombuffer(fbo_read, dtype="f4") * 255)
-                .astype(np.uint8)
-                .reshape(h, w, 4)
-            )
+            graphics.update()
             await asyncio.sleep(0)
 
 
