28e7fb26c5d06f1801ed1f3446746cb938218065
salt-die <salt-die@protonmail.com>
2025-06-22 21:57:22 -0500

    Force shader version. [no ci]

--- examples/advanced/shader_terminal.py
+++ examples/advanced/shader_terminal.py
@@ -15,14 +15,18 @@ from batgrl.app import App
 from batgrl.gadgets.graphics import Graphics, scale_geometry
 
 VERTEX_SHADER = """
-    out vec2 position;
-    void main() {
-        vec2 vertices[3] = vec2[3](vec2(-1, -1), vec2(3, -1), vec2(-1, 3));
-        gl_Position = vec4(vertices[gl_VertexID], 0, 1);
-        position = gl_Position.xy * 0.5 + 0.5;
-    }
+#version 330 core
+
+out vec2 position;
+void main() {
+    vec2 vertices[3] = vec2[3](vec2(-1, -1), vec2(3, -1), vec2(-1, 3));
+    gl_Position = vec4(vertices[gl_VertexID], 0, 1);
+    position = gl_Position.xy * 0.5 + 0.5;
+}
 """
 FRAGMENT_SHADER = """
+#version 330 core
+
 in vec2 position;
 
 uniform float iTime;
