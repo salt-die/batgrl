9cb61f4b3d04664b931941606115a0ef3d66bbed
salt-die <salt-die@protonmail.com>
2025-06-22 22:34:40 -0500

    Formatting: Add newline [no ci]

--- examples/advanced/shader_terminal.py
+++ examples/advanced/shader_terminal.py
@@ -18,6 +18,7 @@ VERTEX_SHADER = """
 #version 330 core
 
 out vec2 position;
+
 void main() {
     vec2 vertices[3] = vec2[3](vec2(-1, -1), vec2(3, -1), vec2(-1, 3));
     gl_Position = vec4(vertices[gl_VertexID], 0, 1);
